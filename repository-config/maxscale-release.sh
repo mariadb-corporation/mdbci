addf9bdfbc3d54a736372dc44cd0666fb0735fa0
Timofey Turenko <timofey.turenko@skysql.com>
2017-11-23 11:24:12 +0200

    fix maxscale ci and release repo generation

--- repository-config/maxscale-release.sh
+++ repository-config/maxscale-release.sh
@@ -6,21 +6,21 @@
 dest=$2
 version=$1
 
-production_repo_prefix=${production_repo_prefix:-"https://downloads.mariadb.com/MaxScale/"}
+production_url=${production_url:-"https://downloads.mariadb.com/MaxScale/"}
 
 dir=`pwd`
 cd ~/mdbci/repository-config/maxscale-release/
 
-mkdir -p $dir/$dest
+mkdir -p $dir/$dest/maxscale-release
 
 export deb_repo_key="135659e928c12247"
-export rpm_repo_key=${production_repo_prefix}/MariaDB-MaxScale-GPG-KEY
+export rpm_repo_key=${production_url}/MariaDB-MaxScale-GPG-KEY
 
 cat old_key_versions | grep "^$version$"
 if [ $? == 0 ]; then
 	echo "old key!"
 	export deb_repo_key="70E4618A8167EE24"
-	export rpm_repo_key=${production_repo_prefix}/old_key.public
+	export rpm_repo_key=${production_url}/old_key.public
 fi
 
 
@@ -28,12 +28,12 @@ export platforms=`ls -1 | grep template | sed "s/\.json\.template//"`
 
 for platform in $platforms
 do
-	export platform_versions=`cat  $platform.platform_version`
+	export platform_versions=`cat $platform.platform_version`
 	for platform_version in ${platform_versions}
 	do
-		  eval "cat <<EOF
-$(<$platform.json.template)
-" 2> /dev/null > $dir/$dest/${platform}_${platform_version}_$version.json
+	  eval "cat <<EOF
+$(<${platform}.json.template)
+" 2> /dev/null > $dir/$dest/maxscale-release/${platform}_${platform_version}_$version.json
 	done
 done
 
