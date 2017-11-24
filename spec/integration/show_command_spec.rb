807eecca7aae2c29e7c315c138d892033f033867
Andrey Vasilyev <andrey@crafted.su>
2017-11-24 11:10:41 +0300

    Merge specifications for show command and show boxinfo commans (refs #23387)

--- spec/integration/show_command_spec.rb
+++ spec/integration/show_command_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'Show command' do
+describe 'show command' do
   execute_shell_commands_and_test_exit_code(
     [
       { shell_command: './mdbci show', exit_code: 0 },
@@ -8,4 +8,16 @@ describe 'Show command' do
       { shell_command: './mdbci show UNREAL', exit_code: 2 },
       { shell_command: './mdbci show box', exit_code: 2 }
     ])
+end
+
+describe 'show boxinfo command' do
+  execute_shell_commands_and_test_exit_code ([
+    {shell_command: './mdbci show boxinfo --box-name ubuntu_trusty_vbox --field platform', exit_code: 0},
+    {shell_command: './mdbci show boxinfo --box-name ubuntu_trusty_vbox', exit_code: 0},
+    {shell_command: './mdbci show boxinfo --box-name ubuntu_trusty_vbox --field', exit_code: 1},
+    {shell_command: './mdbci show boxinfo --box-name ubuntu_trusty_vbox --field WRONG', exit_code: 1},
+    {shell_command: './mdbci show boxinfo --box-name WRONG --field platform', exit_code: 1},
+    {shell_command: './mdbci show boxinfo --box-name WRONG --field WRONG', exit_code: 1},
+    {shell_command: './mdbci show boxinfo --box-name WRONG', exit_code: 1}
+  ])
 end
--- spec/integration_tasks.rb
+++ spec/integration_tasks.rb
@@ -1,7 +1,6 @@
 namespace :run_integration do
 
   task :task_shell_command_testing_example do |t| RakeTaskManager.new(t).run_integration end
-  task :task_6819_show_box_info do |t| RakeTaskManager.new(t).run_integration end
   task :task_6782_show_commands_exit_code do |t| RakeTaskManager.new(t).run_integration end
   task :task_6648_generate_exit_code do |t| RakeTaskManager.new(t).run_integration end
   task :task_show_command do |t| RakeTaskManager.new(t).run_integration end
