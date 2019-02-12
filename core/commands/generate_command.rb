f5018e20a069582febe0eef46a2022a4255dbe6a
Maksim Kosterin <maksim.kosterin@fruct.org>
2019-02-12 13:17:57 +0300

    Move content of else block into the function body (refs #24667)

--- core/commands/generate_command.rb
+++ core/commands/generate_command.rb
@@ -446,6 +446,8 @@ end
       end
     end
     vagrant.puts vagrant_config_footer
+    vagrant.close
+    SUCCESS_RESULT
   rescue RuntimeError => e
     @ui.error(e.message)
     @ui.error('Configuration is invalid')
@@ -453,9 +455,6 @@ end
     vagrant.close
     FileUtils.rm_rf(path)
     ERROR_RESULT
-  else
-    vagrant.close
-    SUCCESS_RESULT
   end
   # rubocop:enable Metrics/MethodLength
 
