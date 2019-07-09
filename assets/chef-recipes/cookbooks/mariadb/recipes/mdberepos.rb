2c19baf94c506b3f9a5df12145dcebfe1aae2e5b
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2019-07-09 13:34:00 +0300

    Use yum_repository resource for mdbe on rhel platforms, fix recipe for suse platforms, add sensitive field (refs #25414)

--- assets/chef-recipes/cookbooks/mariadb/recipes/mdberepos.rb
+++ assets/chef-recipes/cookbooks/mariadb/recipes/mdberepos.rb
@@ -15,37 +15,30 @@ case node[:platform_family]
 when 'debian', 'ubuntu'
   # Split MaxScale repository information into parts
   repository = node['mariadb']['repo'].split(/\s+/)
-  apt_repository 'mariadb' do
+  apt_repository repo_file_name do
     uri repository[0]
     distribution repository[1]
     components repository.slice(2, repository.size)
     keyserver 'keyserver.ubuntu.com'
     key node['mariadb']['repo_key']
+    sensitive true
   end
   apt_update
 when 'rhel', 'fedora', 'centos'
-  # Add the repo
-  template "/etc/yum.repos.d/#{repo_file_name}.repo" do
-    source 'mariadb.rhel.erb'
-    action :create
+  yum_repository repo_file_name do
+    baseurl node['mariadb']['repo']
+    gpgkey node['mariadb']['repo_key']
+    sensitive true
   end
 when 'suse', 'opensuse', 'sles'
   # Add the repo
   template "/etc/zypp/repos.d/#{repo_file_name}.repo" do
     source 'mariadb.suse.erb'
     action :create
+    sensitive true
   end
 
-  execute 'Refreshing repositories (to avoid password issue)' do
-    command 'zypper ref'
-  end
-
-  execute 'Removing mdbe repo (it will be recreated right after removing)' do
-    command "rm /etc/zypp/repos.d/#{repo_file_name}.repo"
-  end
-
-  template "/etc/zypp/repos.d/#{repo_file_name}.repo" do
-    source 'mariadb.suse.erb'
-    action :create
+  execute 'Update zypper cache' do
+    command "zypper refresh"
   end
 end
