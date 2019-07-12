99739534798b5fd110720ac925abbe8dcb1a7b97
Alexey Kallistov <alexey.kallistov@fruct.org>
2019-07-12 09:25:07 +0300

    Add execute method for remove_product class (refs #25421)

--- core/commands/remove_product_command.rb
+++ core/commands/remove_product_command.rb
@@ -7,4 +7,25 @@ class RemoveProductCommand < BaseCommand
     'Installs the product on selected node.'
   end
 
+  def execute
+    if @env.show_help
+      show_help
+      return SUCCESS_RESULT
+    end
+    return ARGUMENT_ERROR_RESULT unless init == SUCCESS_RESULT
+
+    if @mdbci_config.node_names.size != 1
+      @ui.error('Invalid node specified')
+      return ARGUMENT_ERROR_RESULT
+    end
+
+    result = install_product(@mdbci_config.node_names.first)
+
+    if result.success?
+      SUCCESS_RESULT
+    else
+      ERROR_RESULT
+    end
+  end
+
 end
