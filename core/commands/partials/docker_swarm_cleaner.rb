65c2cd81ddf3f0953c087cee05628edabf56ce5e
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2019-07-23 17:20:11 +0300

    Wait for the Docker Swarm to be removed by the Docker (refs #25495)

--- core/commands/partials/docker_swarm_cleaner.rb
+++ core/commands/partials/docker_swarm_cleaner.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../../services/shell_commands'
+require_relative '../../models/result'
 
 # Docker Swarm stack removal utility
 class DockerSwarmCleaner
@@ -15,6 +16,21 @@ class DockerSwarmCleaner
   def destroy_stack(configuration)
     stack_name = configuration.name
     result = run_command_and_log("docker stack rm #{stack_name}")
-    @ui.error("Unable to remove the docker swarm stack #{stack_name}") unless result[:value].success?
+    unless result[:value].success?
+      @ui.error("Unable to remove the Docker swarm stack #{stack_name}")
+      return Result.error("Unable to remove the Docker swarm stack #{stack_name}")
+    end
+
+    100.times do
+      result = run_command("docker stack ps #{stack_name}")
+      unless result[:value].success?
+        @ui.info('Docker stack has been removed')
+        return Result.ok(:ok)
+      end
+
+      sleep(5)
+    end
+
+    @ui.error('Did not manage to wait for the Docker Swarm stack removal')
   end
 end
