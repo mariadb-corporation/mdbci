32aed69a5fedc735054fd039d44043ab3b63d4d2
Alexey Kallistov <alexey.kallistov@fruct.org>
2019-07-26 09:23:46 +0300

    Add selection of platforms for the maxscale purge recipe (refs #25421)

--- assets/chef-recipes/cookbooks/mariadb-maxscale/recipes/purge_maxscale.rb
+++ assets/chef-recipes/cookbooks/mariadb-maxscale/recipes/purge_maxscale.rb
@@ -1,20 +1,43 @@
 # Recipe removes maxscale and it's files from the system
 
-service 'maxscale' do
-  action :stop
-end
 
-package 'maxscale' do
-  action :purge
-end
-
-%w(/var/log/maxscale /run/maxscale /etc/maxscale.modules.d/).each do |path|
-  directory path do
-    action :delete
-    recursive true
+case node[:platform]
+when "debian", "ubuntu", "centos"
+  service 'maxscale' do
+    action :stop
   end
-end
 
-file '/etc/maxscale.cnf' do
-  action :delete
+  package 'maxscale' do
+    action :purge
+  end
+
+  %w(/var/log/maxscale /run/maxscale /etc/maxscale.modules.d/).each do |path|
+    directory path do
+      action :delete
+      recursive true
+    end
+  end
+
+  file '/etc/maxscale.cnf' do
+    action :delete
+  end
+when "suse", nil
+  service 'maxscale' do
+    action :stop
+  end
+
+  package 'maxscale' do
+    action :purge
+  end
+
+  %w(/var/log/maxscale /run/maxscale /etc/maxscale.modules.d/).each do |path|
+    directory path do
+      action :delete
+      recursive true
+    end
+  end
+
+  file '/etc/maxscale.cnf' do
+    action :delete
+  end
 end
