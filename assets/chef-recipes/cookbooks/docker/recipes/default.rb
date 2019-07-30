0af784458c74f8ec20e600c5b8624ee2323e3516
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2019-07-30 14:24:14 +0300

    Add support of centos 6 for docker product (refs #25420)

--- assets/chef-recipes/cookbooks/docker/recipes/default.rb
+++ assets/chef-recipes/cookbooks/docker/recipes/default.rb
@@ -2,8 +2,28 @@
 include_recipe 'packages::default'
 
 # Install Docker
-docker_installation_package 'default' do
-  version node['docker']['version']
-  action :create
+
+if node[:platform_family] == 'rhel' && node[:platform_version].to_i == 7
+  docker_installation_package 'default' do
+    version node['docker']['version']
+    action :create
+  end
+elsif node[:platform_family] == 'rhel' && node[:platform_version].to_i == 6
+  yum_package 'epel-release'
+
+  yum_repository 'docker-repo' do
+    description 'Docker Repo'
+    baseurl 'https://yum.dockerproject.org/repo/main/centos/$releasever/'
+    gpgkey 'https://yum.dockerproject.org/gpg'
+  end
+
+  yum_package 'docker-engine' do
+    version node['docker']['version']
+    action :install
+  end
+
+  service 'docker' do
+    action :start
+  end
 end
 
