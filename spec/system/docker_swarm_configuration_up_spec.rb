61f5431cc64faf1332d80328b04f5a0af6edbbd0
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2019-07-30 10:31:16 +0300

    Use different labels when issuing recreate command (refs #25492)

--- spec/system/docker_swarm_configuration_up_spec.rb
+++ spec/system/docker_swarm_configuration_up_spec.rb
@@ -7,14 +7,15 @@ RSpec.describe 'Docker Swarm configuration', :benchmark do
   it 'should be able to spin-up several times with recreation' do
     labels = %w[backend first second maxscale] * 3
     labels.shuffle!
-    labels.each do |label|
+    labels.each_cons(2) do |first_label, second_label|
       test_dir = Dir.mktmpdir
       config = mdbci_create_configuration(test_dir, 'docker_swarm')
-      result = mdbci_up_command(config, "--labels #{label}")
-      result = mdbci_up_command(config, "--labels #{label} --recreate") if result.success?
+      result = mdbci_up_command(config, "--labels #{first_label}")
+      result = mdbci_up_command(config, "--labels #{second_label} --recreate") if result.success?
       mdbci_destroy_command(config)
       FileUtils.rm_rf(test_dir)
       expect(result).to be_success
     end
   end
 end
+
