b9943673c7192ca52bd1679d24368e0f21c51f11
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2019-08-06 14:44:56 +0300

    Generate the new version of the base file to support consequent calls to the configuration update procedure (refs #25555)

--- core/commands/update_configuration_command.rb
+++ core/commands/update_configuration_command.rb
@@ -17,7 +17,7 @@ class UpdateConfigurationCommand < BaseCommand
     info = <<~HELP
 
       Command allows to update the configuration of the MaxScale and restarts the service.
-      Currently the MDBCI supports only the Docker configuration. The consequent calls to the 
+      Currently the MDBCI supports only the Docker configuration. The consequent calls to the
       up command will use the initial definition of the swarm stack.
 
       When invoking the command you must provide the name of the node that must be updated.
@@ -54,9 +54,9 @@ class UpdateConfigurationCommand < BaseCommand
 
     begin
       @configuration = Configuration.new(@args.first, @env.labels)
-    rescue ArgumentError => error
+    rescue ArgumentError => e
       @ui.error('Unable to detect the configuration')
-      @ui.error(error.message)
+      @ui.error(e.message)
       return ERROR_RESULT
     end
 
@@ -70,17 +70,17 @@ class UpdateConfigurationCommand < BaseCommand
       @ui.error('Please specify path to the new configuration file for the service')
       return ERROR_RESULT
     end
-
-    @docker_config = @configuration.docker_configuration
     SUCCESS_RESULT
   end
   # rubocop:enable Metrics/MethodLength
 
   def update_docker_swarm_stack
+    @new_partial_config = @configuration.docker_configuration
+    @new_docker_config = @configuration.docker_configuration
     @configuration.node_names.each do |node|
       update_node_configuration(node)
     end
-    update_partial_configuration
+    store_configurations
     configurator = DockerSwarmConfigurator.new(@configuration, @env, @ui)
     configurator.configure(generate_partial: false)
   end
@@ -88,23 +88,22 @@ class UpdateConfigurationCommand < BaseCommand
   # Update the configuration of the specified node
   # @param node [String] name of the node to update
   def update_node_configuration(node)
-    node_config = @docker_config['services'][node]
-
+    node_config = @new_partial_config['services'][node]
     current_config_version = node_config['deploy']['labels']['org.mariadb.node.config_version']
-    current_config_label = "#{node}_config_#{current_config_version}"
-    @docker_config['configs'].delete(current_config_label)
-
+    @new_docker_config['services'][node]['deploy']['labels']['org.mariadb.node.config_version'] =
+      current_config_version + 1
+    @new_partial_config['configs'].delete_if { |key, _| key.include?("#{node}_config") }
     config_version = current_config_version + 1
     config_label = "#{node}_config_#{config_version}"
     config_file = copy_configuration_file(node, config_version)
-    @docker_config['configs'][config_label] = { 'file' => config_file }
+    @new_partial_config['configs'][config_label] = { 'file' => config_file }
     node_config['deploy']['labels']['org.mariadb.node.config_version'] = config_version
-    update_node_configuration_link(node, current_config_label, config_label)
+    update_node_configuration_link(node, config_label)
   end
 
-  def update_node_configuration_link(node, current_config_label, config_label)
-    node_configs = @docker_config['services'][node]['configs']
-    node_configs.delete_if { |config| config['source'] == current_config_label }
+  def update_node_configuration_link(node, config_label)
+    node_configs = @new_partial_config['services'][node]['configs']
+    node_configs.delete_if { |config| config['source'].include?("#{node}_config") }
     node_product = @configuration.node_configurations[node]['product']['name']
     target = DockerConfigurationGenerator::CONFIGURATION_LOCATIONS[node_product]
     node_configs.append('source' => config_label, 'target' => target)
@@ -123,7 +122,7 @@ class UpdateConfigurationCommand < BaseCommand
   end
 
   # Update the partial configuration
-  def update_partial_configuration
+  def store_configurations
     if File.exist?(@configuration.docker_partial_configuration_path)
       partial_config = YAML.load_file(@configuration.docker_partial_configuration_path)
       required_service_names = partial_config['services'].keys.concat(@configuration.node_names).uniq
@@ -131,8 +130,8 @@ class UpdateConfigurationCommand < BaseCommand
       required_service_names = @configuration.node_names
     end
 
-    new_partial_config = Marshal.load(Marshal.dump(@docker_config))
-    new_partial_config['services'].keep_if { |service_name, _| required_service_names.include?(service_name) }
-    File.write(@configuration.docker_partial_configuration_path, YAML.dump(new_partial_config))
+    @new_partial_config['services'].keep_if { |service_name, _| required_service_names.include?(service_name) }
+    File.write(@configuration.docker_partial_configuration_path, YAML.dump(@new_partial_config))
+    File.write(@configuration.docker_configuration_path, YAML.dump(@new_docker_config))
   end
 end
