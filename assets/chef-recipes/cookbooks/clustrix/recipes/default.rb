a2408962e4ccc1398fb3dd61f6dbcf811b60974a
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2019-08-16 14:24:36 +0300

    Use ENV to get SUDO_USER and use Dir.home method, add activation of clustrix product (refs #25213)

--- assets/chef-recipes/cookbooks/clustrix/recipes/default.rb
+++ assets/chef-recipes/cookbooks/clustrix/recipes/default.rb
@@ -6,54 +6,41 @@ yum_package 'epel-release'
 # Install Operating System Dependent Packages
 case node[:platform_family]
 when 'rhel', 'fedora', 'centos'
-  yum_package %w(bzip2 wget screen vim htop mdadm ntp ntpdate)
+  # TODO: Remove vim, ntp, ntpdate
+  yum_package %w(yum-utils bzip2 wget screen vim mdadm ntp ntpdate)
   # If platform is RHEL, edit the following repo file
   if node[:platform] == 'redhat'
     execute 'Edit the repo file' do
       command 'yum-config-manager --enable rhui-REGION-rhel-server-optional'
-      returns [0, 70]
     end
-  end
-  service 'ntpd' do
-    action :enable
-  end
-  service 'ntpd' do
-    action :start
-  end
-  service 'firewalld' do
-    action :disable
-  end
-  service 'firewalld' do
-    action :stop
-  end
-  execute 'Disable SELinux Temporarily' do
-    command 'setenforce 0'
-  end
-  ruby_block 'Disable SELinux Permanently' do
-    block do
-      selinux = Chef::Util::FileEdit.new('/etc/sysconfig/selinux')
-      selinux.search_file_replace_line(/^SELINUX=.+$/, 'SELINUX=disabled')
-      selinux.write_filentpd
-    end
-    only_if { File.exist?('/etc/sysconfig/selinux') }
   end
 end
 
+user = ENV['SUDO_USER']
+home_dir = Dir.home(user)
+
 # Download ClustrixDB installer
-remote_file '/home/vagrant/clustrix-installer.tar.bz2' do
+remote_file File.join(home_dir, 'clustrix-installer.tar.bz2') do
   source node['clustrix']['repo']
   action :create
 end
 
 # Create directory to unpack clustrix-installer
-directory '/home/vagrant/clustrix-installer'
+directory File.join(home_dir, 'clustrix-installer')
 
 execute 'Unpack ClustrixDB installer' do
-  command 'tar xvjf /home/vagrant/clustrix-installer.tar.bz2 -C /home/vagrant/clustrix-installer --strip-components=1'
+  command "tar xvjf #{File.join(home_dir, 'clustrix-installer.tar.bz2')}"\
+          " -C #{File.join(home_dir, 'clustrix-installer')} --strip-components=1"
 end
 
 execute 'Install ClustrixDB via unpacked installer' do
   command './clxnode_install.py --yes --force'
-  cwd '/home/vagrant/clustrix-installer/'
+  cwd File.join(home_dir, 'clustrix-installer')
+  # TODO: Remove
   returns [0, 4]
+end
+
+execute 'Set ClustrixDB license' do
+  command "mysql -e \"#{node['clustrix']['license']}\""
+  sensitive true
 end
