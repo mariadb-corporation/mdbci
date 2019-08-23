5863abc96d701d6e3f8767fce3915440bd178f77
Alexey Kallistov <alexey.kallistov@fruct.org>
2019-08-23 13:02:35 +0300

    Add removal of the old version of docker for debian and ubuntu (refs #25644)

--- core/commands/setup_dependencies_command.rb
+++ core/commands/setup_dependencies_command.rb
@@ -348,6 +348,7 @@ class DebianDependencyManager < DependencyManager
   end
 
   def install_docker
+    run_command("sudo apt-get remove docker docker-engine docker.io containerd runc")
     run_command("curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -")
     run_command("sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/debian  $(lsb_release -cs)  stable")
     run_command("sudo apt-get update")
