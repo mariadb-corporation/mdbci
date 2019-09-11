c184dbbfafbf84fbc0199790651d5ae12a9083e3
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2019-09-11 11:33:28 +0300

    Allow build script ot be run from any directory (refs #25740)

--- package/build.sh
+++ package/build.sh
@@ -2,7 +2,7 @@
 # This script bundles the mdbci as the AppImage.
 # You should pass the build version as the parameter to the script.
 # Resulting file will reside in build/out subdirectory
-set -e
+set -xe
 
 BUILD_VERSION=$1
 
@@ -14,9 +14,8 @@ EOF
   exit 1
 fi
 
-CURRENT_DIR="$(pwd)"
-
-BUILD_DIR="$(pwd)/build"
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+BUILD_DIR="$CURRENT_DIR/build"
 if [ -d "$BUILD_DIR" ]; then
   rm -rf "$BUILD_DIR"
 fi
@@ -25,12 +24,15 @@ mkdir -p "$BUILD_DIR"
 # Copy all files that should be distributed to the build directory
 MDBCI_BUILD_DIR="$BUILD_DIR/mdbci"
 mkdir -p "$MDBCI_BUILD_DIR"
+pushd "$CURRENT_DIR/.."
 for file in assets config confs core docs Gemfile Gemfile.lock mdbci scripts
 do
-  cp -r "../$file" "$MDBCI_BUILD_DIR/"
+  cp -r "$file" "$MDBCI_BUILD_DIR/"
 done
+popd
 
 # Copy all files required by ruby.appimage
+pushd "$CURRENT_DIR"
 for file in mdbci.desktop mdbci.png mdbci.sh
 do
   cp "$file" "$BUILD_DIR/"
@@ -38,9 +40,10 @@ done
 
 # Copy the runner directory to the build
 cp -r "runner" "$BUILD_DIR/"
+popd
 
 # Put the version information into the build
-pushd ..
+pushd "$CURRENT_DIR/.."
 version=$(git rev-parse HEAD)
 time=$(date +%F)
 echo "${BUILD_VERSION}, ${version}, ${time}" > "$MDBCI_BUILD_DIR/version"
