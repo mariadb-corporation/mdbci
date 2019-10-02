45c8657d4923a2384ad64da0ddb55e728cc696a5
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2019-10-02 10:08:34 +0300

    Update packages and chrony recepies to be run with OpenSuse 15 (refs #25779)

--- assets/chef-recipes/cookbooks/packages/recipes/default.rb
+++ assets/chef-recipes/cookbooks/packages/recipes/default.rb
@@ -11,16 +11,9 @@ include_recipe 'packages::configure_apt'
 
 # install additional packages for all platform
 %w(net-tools psmisc curl rsync).each do |pkg|
-  if platform?("linux") # SLES 15 is detected as the Linux platform.
-    zypper_package pkg do
-      retries 2
-      retry_delay 10
-    end
-  else
-    package pkg do
-      retries 2
-      retry_delay 10
-    end
+  package pkg do
+    retries 2
+    retry_delay 10
   end
 end
 
