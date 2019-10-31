b3633cd29730af731c8449069739fbfcf05d5b9f
Alexey Kallistov <alexey.kallistov@fruct.org>
2019-10-31 14:37:23 +0300

    Add execute methdof for show_command class (refs #26023)

--- core/commands/show_command.rb
+++ core/commands/show_command.rb
@@ -3,4 +3,21 @@
 
 class ShowCommand < BaseCommand
 
+  def execute
+    if @args.empty?
+      $out.warning 'Please specify an action for the show command.'
+      display_usage_info('show', SHOW_COMMAND_ACTIONS)
+      return 0
+    end
+    action_name, *action_parameters = *@args
+    action = SHOW_COMMAND_ACTIONS[action_name.to_sym]
+    if action.nil?
+      $out.warning "Unknown action for the show command: #{action_name}."
+      display_usage_info('show', SHOW_COMMAND_ACTIONS)
+      return 2
+    end
+    instance_exec(*action_parameters, &action[:action])
+  end
+
+
 end
