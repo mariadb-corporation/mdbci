131f981ed8197345d9a94643b1657558dfe8fe07
Alexey Kallistov <alexey.kallistov@fruct.org>
2019-11-08 17:07:41 +0300

    Add within_ssh_session method for ssh_command module (refs #25741)

--- core/services/ssh_command.rb
+++ core/services/ssh_command.rb
@@ -1,5 +1,15 @@
 
 
 module SshCommand
-
+  # Connect to the specified machine and yield active connection
+  # @param machine [Hash] information about machine to connect
+  def self.within_ssh_session(machine)
+    options = Net::SSH.configuration_for(machine['network'], true)
+    options[:auth_methods] = %w[publickey none]
+    options[:verify_host_key] = :never
+    options[:keys] = [machine['keyfile']]
+    Net::SSH.start(machine['network'], machine['whoami'], options) do |ssh|
+      yield ssh
+    end
+  end
 end
