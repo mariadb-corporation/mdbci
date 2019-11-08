71b48d7457af61daa6c392b299d6137875bdb7ea
Alexey Kallistov <alexey.kallistov@fruct.org>
2019-11-08 16:03:34 +0300

    Add resource address constants for network_configurator class (refs #25741)

--- core/services/network_configurator.rb
+++ core/services/network_configurator.rb
@@ -3,6 +3,12 @@ require 'net/ssh'
 
 # Class allows to configure a specified machine
 class NetworkConfigurator
+
+  GITHUB = 'https://github.com'
+  APACHE = 'https://www-eu.apache.org/'
+  NODEJS = 'https://nodejs.org/'
+  SOURCEFORGE = 'http://prdownloads.sourceforge.net/'
+
   def initialize(logger)
     @logger = logger
   end
