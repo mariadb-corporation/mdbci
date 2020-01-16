bfe970843917a98b758704414ac28933d7c014f1
Dmitriy Karpovskiy <dmitriy.karpovskiy@fruct.org>
2020-01-16 13:44:38 +0300

    Update check_relevance method for check_relevance_command (refs #26333)

--- core/commands/check_relevance_command.rb
+++ core/commands/check_relevance_command.rb
@@ -6,51 +6,41 @@ require_relative '../services/machine_configurator'
 
 # This class checks relevance network config
 class CheckRelevanceCommand < BaseCommand
+  AVAILABLE_TIME = 5
   def self.synopsis
     'Check for relevance of network_config file.'
   end
 
   def show_help
     info = <<-HELP
-'check_relevance' command check .
+Check relevance command tries to connect to all nodes in the _network_config file.
+_network_config file is relevant, if the connection was successful to all nodes.
     HELP
     @ui.info(info)
   end
 
   def execute
+    if @env.show_help
+      show_help
+      return SUCCESS_RESULT
+    end
     check_relevance
-      # system 'scripts/check_network_config.sh ' + @args
   end
 
   def check_relevance
-    pp File.file?(@args)
-    z = NetworkSettings.from_file(@args).node_name_list
-    pp z
-    machine = MachineConfigurator.new(@logger)
-    machine.run_command(z[0], "AS")
-    pp 1
+    return Result.error("#{@args} does not exist") unless File.file?(@args)
 
-    #{return false unless @args.length < 16
-    #path_to_config = @args.sub("_network_config", "/.vagrant/machines")
-    #access_time = File.atime(@args)
-    #modify_time = File.mtime(@args)
-    #network_config_time = access_time if access_time > modify_time
-    #network_config_time = modify_time if modify_time >= access_time
-    #is_relevance = 1
-    #File.directory?(path_to_config)
-    #nodes = Dir["#{path_to_config}/**/**/synced_folders"]
-    #nodes.empty?
-    #accessed = []
-    #modified = []
-    #nodes.each do |node|
-    #  accessed.push(File.atime(node))
-    #  modified.push(File.mtime(node))
-    #end
-    #accessed.each do |time|
-    #  false if time > network_config_time
-    #end
-    #modified.each do |time|
-    #  false if time > network_config_time
-    #  end
+    network_settings = NetworkSettings.from_file(@args)
+    all_nodes = network_settings.node_name_list
+    machine = MachineConfigurator.new(@ui)
+    all_nodes.each do |node|
+      Timeout.timeout(AVAILABLE_TIME) do
+        machine.run_command(network_settings.node_settings(node), '')
+      end
+    rescue Errno::EHOSTUNREACH || Timeout::Error
+      return Result.error("#{@args} is not relevance")
+    end
+    @ui.info("#{@args} is relevance")
+    Result.ok("#{@args} is relevance")
   end
 end
