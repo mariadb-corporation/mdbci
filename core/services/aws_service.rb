24d39478a85178cd4bbe56de2e7bceb641b44fd1
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2020-01-16 15:39:44 +0300

    Initialize gcp and aws services in the Session class in case non existing them tool configurations, return empty data from it services if it is not configured (refs #26357)

--- core/services/aws_service.rb
+++ core/services/aws_service.rb
@@ -24,23 +24,37 @@ class AwsService
   end
 
   def initialize(aws_config, logger)
+    @logger = logger
+    if aws_config.nil?
+      @configured = false
+      return
+    end
+
     @client = Aws::EC2::Client.new(
       access_key_id: aws_config['access_key_id'],
       secret_access_key: aws_config['secret_access_key'],
       region: aws_config['region']
     )
-    @logger = logger
+    @configured = true
+  end
+
+  def configured?
+    @configured
   end
 
   # Get information about instances
   # @return [Hash] instances information
   def describe_instances
+    return { reservations: [] } unless configured?
+
     @client.describe_instances.to_h
   end
 
   # Get the instances list
   # @return [Array] instances list in format [{ instance_id, node_name, configuration_id }]
   def instances_list
+    return [] unless configured?
+
     describe_instances[:reservations].map do |reservation|
       reservation[:instances].map do |instance|
         next nil if !%w[running pending].include?(instance[:state][:name]) || instance[:tags].nil?
@@ -55,6 +69,8 @@ class AwsService
   # Get the vpc list
   # @return [Array] vpc list in format [{ vpc_id, configuration_id }]
   def vpc_list(tags)
+    return [] unless configured?
+
     @client.describe_vpcs(filters: tags_to_filters(tags)).to_h[:vpcs].map do |vpc|
       configuration_id = vpc[:tags].find { |tag| tag[:key] == 'configuration_id' }&.fetch(:value, nil)
       { vpc_id: vpc[:vpc_id], configuration_id: configuration_id }
@@ -64,13 +80,15 @@ class AwsService
   # Get the vpc specified by the configuration id
   # @param [String] configuration_id configuration id
   def get_vpc_by_config_id(configuration_id)
+    return nil unless configured?
+
     vpc_list(configuration_id: configuration_id).first
   end
 
   # Delete vpc specified by the vpc id
   # @param [String] vpc_id vpc id
   def delete_vpc(vpc_id)
-    return if vpc_id.nil?
+    return if vpc_id.nil? || !configured?
 
     @client.delete_vpc(vpc_id: vpc_id)
   end
@@ -78,7 +96,7 @@ class AwsService
   # Delete vpc specified by the configuration id
   # @param [String] configuration_id configuration id
   def delete_vpc_by_config_id(configuration_id)
-    return if configuration_id.nil?
+    return if configuration_id.nil? || !configured?
 
     vpc = get_vpc_by_config_id(configuration_id)
     delete_vpc(vpc[:vpc_id]) unless vpc.nil?
@@ -87,6 +105,8 @@ class AwsService
   # Get the security_group list
   # @return [Array] security_group list in format [{ group_id, configuration_id }]
   def security_group_list(tags)
+    return [] unless configured?
+
     @client.describe_security_groups(filters: tags_to_filters(tags)).to_h[:security_groups].map do |security_group|
       configuration_id = security_group[:tags].find { |tag| tag[:key] == 'configuration_id' }&.fetch(:value, nil)
       { group_id: security_group[:group_id], configuration_id: configuration_id }
@@ -96,13 +116,15 @@ class AwsService
   # Get the security group specified by the configuration id
   # @param [String] configuration_id configuration id
   def get_security_group_by_config_id(configuration_id)
+    return nil unless configured?
+
     security_group_list(configuration_id: configuration_id).first
   end
 
   # Delete security group specified by the group id
   # @param [String] group_id group id
   def delete_security_group(group_id)
-    return if group_id.nil?
+    return if group_id.nil? || !configured?
 
     @client.delete_security_group(group_id: group_id)
   end
@@ -110,7 +132,7 @@ class AwsService
   # Delete security group specified by the configuration id
   # @param [String] configuration_id configuration id
   def delete_security_group_by_config_id(configuration_id)
-    return if configuration_id.nil?
+    return if configuration_id.nil? || !configured?
 
     security_group = get_security_group_by_config_id(configuration_id)
     delete_security_group(security_group[:group_id]) unless security_group.nil?
@@ -119,7 +141,7 @@ class AwsService
   # Delete key pair specified by it name
   # @param [String] key_name key pair name
   def delete_key_pair(key_name)
-    return if key_name.nil?
+    return if key_name.nil? || !configured?
 
     @client.delete_key_pair(key_name: key_name)
   end
@@ -128,6 +150,8 @@ class AwsService
   #
   # @return [Array] instances names list.
   def instances_names_list
+    return [] unless configured?
+
     aws_instances_ids = instances_list || []
     aws_instances_ids.map { |instance| instance[:node_name] }
   end
@@ -136,7 +160,7 @@ class AwsService
   # @param [String] instance_id to check
   # @return [Boolean] true if it is running
   def instance_running?(instance_id)
-    return false if instance_id.nil?
+    return false if instance_id.nil? || !configured?
 
     response = @client.describe_instance_status(instance_ids: [instance_id])
     response.instance_statuses.any? do |status|
@@ -149,6 +173,8 @@ class AwsService
   # @param [String] instance_name to check
   # @return [Boolean] true if it is running
   def instance_by_name_running?(instance_name)
+    return false unless configured?
+
     instance_running?(get_aws_instance_id_by_node_name(instance_name))
   end
 
@@ -157,13 +183,15 @@ class AwsService
   # @param [String] instance_name to check
   # @return [Boolean] true if it is running
   def instance_by_config_id_running?(configuration_id, instance_name)
+    return false unless configured?
+
     instance_running?(get_aws_instance_id_by_config_id(configuration_id, instance_name))
   end
 
   # Terminate instance specified by the unique identifier
   # @param [String] instance_id to terminate
   def terminate_instance(instance_id)
-    return if instance_id.nil?
+    return if instance_id.nil? || !configured?
 
     @client.terminate_instances(instance_ids: [instance_id])
     nil
@@ -172,6 +200,8 @@ class AwsService
   # Terminate instance specified by the node name
   # @param [String] node_name name of node to terminate
   def terminate_instance_by_name(node_name)
+    return unless configured?
+
     terminate_instance(get_aws_instance_id_by_node_name(node_name))
   end
 
@@ -179,6 +209,8 @@ class AwsService
   # @param [String] configuration_id configuration id
   # @param [String] node_name name of node to terminate
   def terminate_instance_by_config_id(configuration_id, node_name)
+    return unless configured?
+
     terminate_instance(get_aws_instance_id_by_config_id(configuration_id, node_name))
   end
 
@@ -187,6 +219,8 @@ class AwsService
   # @param node_name [String] name of instance.
   # @return [String] id of the instance.
   def get_aws_instance_id_by_node_name(node_name)
+    return nil unless configured?
+
     found_instance = instances_list.find { |instance| instance[:node_name] == node_name }
     found_instance.nil? ? nil : found_instance[:instance_id]
   end
@@ -197,6 +231,8 @@ class AwsService
   # @param node_name [String] name of instance.
   # @return [String] id of the instance.
   def get_aws_instance_id_by_config_id(configuration_id, node_name)
+    return nil unless configured?
+
     found_instance = instances_list.find do |instance|
       instance[:node_name] == node_name &&
         instance[:configuration_id] == configuration_id
--- core/services/gcp_service.rb
+++ core/services/gcp_service.rb
@@ -8,19 +8,31 @@ class GcpService
   SCOPE = %w[https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/compute]
 
   def initialize(gcp_config, logger)
-    @gcp_config = gcp_config
     @logger = logger
+    if gcp_config.nil?
+      @configured = false
+      return
+    end
+
+    @gcp_config = gcp_config
     @service = Google::Apis::ComputeV1::ComputeService.new
     @service.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
       json_key_io: File.open(@gcp_config['credentials_file']),
       scope: SCOPE
     )
     @service.authorization.fetch_access_token!
+    @configured = true
+  end
+
+  def configured?
+    @configured
   end
 
   # Fetch instances list and return instance names.
   # @return [Array<String>] instance names.
   def instances_list
+    return [] unless configured?
+
     @service.fetch_all do |token|
       @service.list_instances(@gcp_config['project'], @gcp_config['zone'], page_token: token)
     end.map(&:name)
@@ -29,24 +41,32 @@ class GcpService
   # Returns false if a new vpc resources need to be generated for the current configuration, otherwise true.
   # @return [Boolean] result.
   def use_existing_network?
+    return false unless configured?
+
     @gcp_config['use_existing_network']
   end
 
   # Delete instance specified by the it name
   # @param instance_name [String] name of the instance to delete.
   def delete_instance(instance_name)
+    return unless configured?
+
     @service.delete_instance(@gcp_config['project'], @gcp_config['zone'], instance_name)
   end
 
   # Delete network specified by the it name
   # @param network_name [String] name of the network to delete.
   def delete_network(network_name)
+    return unless configured?
+
     @service.delete_network(@gcp_config['project'], network_name)
   end
 
   # Delete firewall specified by the it name
   # @param firewall_name [String] name of the firewall to delete.
   def delete_firewall(firewall_name)
+    return unless configured?
+
     @service.delete_firewall(@gcp_config['project'], firewall_name)
   end
 end
