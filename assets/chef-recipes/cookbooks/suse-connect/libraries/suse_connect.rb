5797301cb7cf34e85edfb88fe84fd085bdb2bc5f
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2020-03-03 13:07:06 +0300

    Move ruby code in the suse_connect recipe to ruby_block resource (refs #26614)

--- assets/chef-recipes/cookbooks/suse-connect/libraries/suse_connect.rb
+++ assets/chef-recipes/cookbooks/suse-connect/libraries/suse_connect.rb
@@ -1,33 +1,35 @@
 class Chef
   class Recipe
-    # Move specified products to begin of products list if
-    # it products exists in list.
-    #
-    # @param product_names [Array<String>] the names of the products to be moved to the top of the list.
-  #                                        The name order should be in the order in which
-    #                                      the product was activated
-    # @param products [Array<Hash>] list of product information obtained from the `SUSEConnect --status` command
-    # @return [Array<Hash>] reordered products list.
-    def move_products_to_begin(product_names, products)
-      result = products
-      product_names.reverse_each do |product_name|
-        product = result.find { |p| p['identifier'] == product_name }
-        unless product.nil?
-          result.delete(product)
-          result.unshift(product)
+    module SuseConnectHelpers
+      # Move specified products to begin of products list if
+      # it products exists in list.
+      #
+      # @param product_names [Array<String>] the names of the products to be moved to the top of the list.
+      #                                      The name order should be in the order in which
+      #                                      the product was activated
+      # @param products [Array<Hash>] list of product information obtained from the `SUSEConnect --status` command
+      # @return [Array<Hash>] reordered products list.
+      def self.move_products_to_begin(product_names, products)
+        result = products
+        product_names.reverse_each do |product_name|
+          product = result.find { |p| p['identifier'] == product_name }
+          unless product.nil?
+            result.delete(product)
+            result.unshift(product)
+          end
         end
+        result
       end
-      result
-    end
 
-    # Remove specified product from products list if
-    # it product exists in list.
-    #
-    # @param product_name [String] the names of the product to remove
-    # @param products [Array<Hash>] list of product information obtained from the `SUSEConnect --status` command
-    # @return [Array<Hash>] the resulting list of products.
-    def remove_product(product_name, products)
-      products.reject { |product| product['identifier'] == product_name }
+      # Remove specified product from products list if
+      # it product exists in list.
+      #
+      # @param product_name [String] the names of the product to remove
+      # @param products [Array<Hash>] list of product information obtained from the `SUSEConnect --status` command
+      # @return [Array<Hash>] the resulting list of products.
+      def self.remove_product(product_name, products)
+        products.reject { |product| product['identifier'] == product_name }
+      end
     end
   end
 end
