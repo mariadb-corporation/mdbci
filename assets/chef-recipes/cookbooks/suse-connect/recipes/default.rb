02efb42fd06c664612e7a4b1bd2a8f316e19d863
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2020-03-03 14:53:57 +0300

    Add the force option to rm /etc/SUSEConnect in the suse-connect recipe (refs #26614)

--- assets/chef-recipes/cookbooks/suse-connect/recipes/default.rb
+++ assets/chef-recipes/cookbooks/suse-connect/recipes/default.rb
@@ -3,7 +3,7 @@ execute 'Cleanup registration' do
 end
 
 CLEANUP_COMMANDS = [
-  'rm /etc/SUSEConnect',
+  'rm -f /etc/SUSEConnect',
   'rm -f /etc/zypp/{repos,services,credentials}.d/*',
   'rm -f /usr/lib/zypp/plugins/services/*',
   "sed -i '/^# Added by SMT reg/,+1d' /etc/hosts",
