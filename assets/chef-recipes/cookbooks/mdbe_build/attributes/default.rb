971708b1b28788fdb3ff8bead64c21bf3127fdc1
Dmitriy Karpovskiy <dmitriy.karpovskiy@fruct.org>
2020-03-12 00:26:36 +0300

    Add an array to store packages and add a mysql user (refs #26581)

--- assets/chef-recipes/cookbooks/mdbe_build/attributes/default.rb
+++ assets/chef-recipes/cookbooks/mdbe_build/attributes/default.rb
@@ -1,14 +1,4 @@
+# frozen_string_literal: true
 
 # CMake version
 default['mdbe_build']['cmake_version'] = '3.16.4'
-
-# Source list for Ubuntu Bionic
-default['mdbe_build']['bionic_sources_list'] = 'deb mirror://mirrors.ubuntu.com/mirrors.txt bionic main restricted universe multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt bionic-updates main restricted universe multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt bionic-backports main restricted universe multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt bionic-security main restricted universe multiverse
-
-deb-src mirror://mirrors.ubuntu.com/mirrors.txt bionic main restricted universe multiverse
-deb-src mirror://mirrors.ubuntu.com/mirrors.txt bionic-updates main restricted universe multiverse
-deb-src mirror://mirrors.ubuntu.com/mirrors.txt bionic-backports main restricted universe multiverse
-deb-src mirror://mirrors.ubuntu.com/mirrors.txt bionic-security main restricted universe multiverse'
