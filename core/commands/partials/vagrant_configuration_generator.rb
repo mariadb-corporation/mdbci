013d52fd2e95755eb6cbb194b108140e5b57f794
Dmitriy Karpovskiy <dmitriy.karpovskiy@fruct.org>
2020-04-09 18:26:27 +0300

    Add the xpand product and add the Attributes Generator module which creates attributes for chef recipes (refs #26783)

--- core/commands/partials/vagrant_configuration_generator.rb
+++ core/commands/partials/vagrant_configuration_generator.rb
@@ -12,8 +12,7 @@ require_relative '../base_command'
 require_relative '../../out'
 require_relative '../../models/configuration.rb'
 require_relative '../../services/shell_commands'
-require_relative '../../../core/services/configuration_generator'
-require_relative '../../services/product_attributes'
+require_relative '../../services/attributes_generator'
 
 # The class generates the MDBCI configuration for use in pair with the Vagrant backend
 # rubocop:disable Metrics/ClassLength
@@ -143,117 +142,6 @@ DNSStubListener=yes" > /etc/systemd/resolved.conf
   end
   # rubocop:enable Metrics/MethodLength
 
-  # Make product config and recipe name for install it to the VM.
-  #
-  # @param product [Hash] parameters of product to configure from configuration file
-  # @param box [String] name of the box
-  # @return [Result::Base] recipe name and product config in format { recipe: String, config: Hash }.
-  # rubocop:disable Metrics/MethodLength
-  def make_product_config_and_recipe_name(product, box)
-    repo = nil
-    if !product['repo'].nil?
-      repo_name = product['repo']
-      @ui.info("Repo name: #{repo_name}")
-      return Result.error("Unknown key for repo #{repo_name} will be skipped") unless @env.repos.knownRepo?(repo_name)
-
-      @ui.info("Repo specified [#{repo_name}] (CORRECT), other product params will be ignored")
-      repo = @env.repos.getRepo(repo_name)
-      product_name = @env.repos.productName(repo_name)
-    else
-      product_name = product['name']
-    end
-    recipe_name = ProductAttributes.recipe_name(product_name)
-    ConfigurationGenerator
-      .generate_product_config(@env.repos, product_name, product, box, repo, @provider)
-      .and_then do |product_config|
-        @ui.info("Recipe #{recipe_name}")
-        Result.ok(recipe: recipe_name, config: product_config)
-      end
-  end
-  # rubocop:enable Metrics/MethodLength
-
-  # Initialize product configs Hash and recipe names list with necessary recipes
-  # and configurations on which the box depends.
-  #
-  # @param box [String] name of the box
-  # @return [Result::Base] Hash in format { recipe_names: [], product_configs: {} }
-  def init_product_configs_and_recipes(box)
-    product_configs = {}
-    recipe_names = []
-
-    if @env.box_definitions.get_box(box)['configure_subscription_manager'] == 'true'
-      return Result.error('Credentials for Red Hat Subscription-Manager are not configured') if @env.rhel_config.nil?
-
-      recipe_names << 'subscription-manager'
-      product_configs.merge!('subscription-manager': @env.rhel_config)
-    end
-
-    if @env.box_definitions.get_box(box)['configure_suse_connect'] == 'true'
-      return Result.error('Credentials for SUSEConnect are not configured') if @env.suse_config.nil?
-
-      recipe_names << 'suse-connect'
-      product_configs.merge!('suse-connect': @env.suse_config)
-    end
-
-    recipe_names << 'packages'
-    Result.ok({ product_configs: product_configs, recipe_names: recipe_names })
-  end
-
-  # Generate the role description for the specified node.
-  #
-  # @param name [String] internal name of the machine specified in the template
-  # @param products [Array<Hash>] list of parameters of products to configure from configuration file
-  # @param box [String] name of the box
-  # @return [Result::Base<String>] pretty formatted role description in JSON format
-  def get_role_description(name, products, box)
-    recipes_result = init_product_configs_and_recipes(box)
-    return recipes_result if recipes_result.error?
-
-    product_configs = recipes_result.value[:product_configs]
-    recipe_names = recipes_result.value[:recipe_names]
-    products.each do |product|
-      recipe_and_config_result = make_product_config_and_recipe_name(product, box)
-      return recipe_and_config_result if recipe_and_config_result.error?
-
-      recipe_and_config_result.and_then do |recipe_and_config|
-        product_configs.merge!(recipe_and_config[:config])
-        recipe_names << recipe_and_config[:recipe]
-      end
-    end
-    role_description = ConfigurationGenerator.generate_role_json_description(name, recipe_names, product_configs)
-    Result.ok(role_description)
-  end
-
-  # Check for the existence of a path, create it if path is not exists or clear path
-  # if it is exists and override parameter is true.
-  #
-  # @param path [String] path of the configuration file
-  # @param override [Bool] clean directory if it is already exists
-  # @return [Bool] false if directory path is already exists and override is false, otherwise - true.
-  def check_path(path, override)
-    if Dir.exist?(path) && !override
-      @ui.error("Folder already exists: #{path}. Please specify another name or delete")
-      return false
-    end
-    FileUtils.rm_rf(path)
-    Dir.mkdir(path)
-    true
-  end
-
-  # Check for MDBCI node names defined in the template to be valid Ruby object names.
-  #
-  # @param config [Hash] value of the configuration file
-  # @return [Bool] true if all nodes names are valid, otherwise - false.
-  def check_nodes_names(config)
-    invalid_names = config.map do |node|
-      (node[0] =~ /^[a-zA-Z_]+[a-zA-Z_\d]*$/).nil? ? node[0] : nil
-    end.compact
-    return true if invalid_names.empty?
-
-    @ui.error("Invalid nodes names: #{invalid_names}. "\
-              'Nodes names defined in the template to be valid Ruby object names.')
-    false
-  end
 
   # Check for the box emptiness and existence of a box in the boxes list.
   #
@@ -309,14 +197,6 @@ DNSStubListener=yes" > /etc/systemd/resolved.conf
     end
   end
 
-  # Parse the products lists from configuration of node.
-  #
-  # @param node [Array] internal name of the machine specified in the template
-  # @return [Array<Hash>] list of parameters of products.
-  def parse_products_info(node)
-    [].push(node[1]['product']).push(node[1]['products']).flatten.compact.uniq
-  end
-
   # Make a list of node parameters, create the role and node_config files, generate
   # node definition for the Vagrantfile.
   #
@@ -329,12 +209,12 @@ DNSStubListener=yes" > /etc/systemd/resolved.conf
   def node_definition(node, path, cookbook_path)
     box = node[1]['box'].to_s
     unless box.empty?
-      node_params = make_node_params(node, @boxes.get_box(box))
+      node_params = AttributesGenerator.make_node_params(node, @boxes.get_box(box), :vagrant, @env)
       print_node_info(node_params, box)
     end
-    products = parse_products_info(node)
+    products = AttributesGenerator.parse_products_info(node)
     @ui.info("Machine #{node_params[:name]} is provisioned by #{products}")
-    get_role_description(node_params[:name], products, box).and_then do |role|
+    AttributesGenerator.get_role_description(node_params[:name], products, box, @ui, @env).and_then do |role|
       IO.write(self.class.role_file_name(path, node_params[:name]), role)
       IO.write(self.class.node_config_file_name(path, node_params[:name]),
                JSON.pretty_generate('run_list' => ["role[#{node_params[:name]}]"]))
@@ -393,7 +273,7 @@ DNSStubListener=yes" > /etc/systemd/resolved.conf
   # otherwise - ERROR_RESULT or ARGUMENT_ERROR_RESULT.
   def generate(path, config, override, provider)
     # TODO: MariaDb Version Validator
-    checks_result = check_path(path, override) && check_nodes_names(config)
+    checks_result = AttributesGenerator.check_path(path, override, @ui) && AttributesGenerator.check_nodes_names(config, @ui)
     return ARGUMENT_ERROR_RESULT unless checks_result
 
     cookbook_path = if config['cookbook_path'].nil?
