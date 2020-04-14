888c8b255b39d869b08bf33806c5852dc284b693
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2020-04-14 12:39:52 +0300

    Fix Rubocop errors in ConfigurationTemplate class (refs #26744)

--- core/models/configuration_template.rb
+++ core/models/configuration_template.rb
@@ -20,14 +20,14 @@ class ConfigurationTemplate
   # Read the contents of the template file. Raise exceptions if something is missing
   def read_template_file
     unless File.exist?(@template_path)
-      raise("The specified template file '#{@template_path}' does not exist. Please specify correct path.")
+      raise "The specified template file '#{@template_path}' does not exist."
     end
 
     begin
       instance_config_file = File.read(@template_path)
       JSON.parse(instance_config_file)
-    rescue IOError, JSON::ParserError => error
-      raise("The configuration file '#{@template_path}' is not valid. Error: #{error.message}")
+    rescue IOError, JSON::ParserError => e
+      raise "The configuration file '#{@template_path}' is not valid. Error: #{e.message}"
     end
   end
 
@@ -40,22 +40,23 @@ class ConfigurationTemplate
   end
 
   TEMPLATE_TYPE_BY_PROVIDER = {
-      dedicated: %w[dedicated],
-      docker: %w[docker],
-      terraform: %w[aws digitalocean gcp],
-      vagrant: %w[libvirt vbox],
-  }
+    dedicated: %w[dedicated],
+    docker: %w[docker],
+    terraform: %w[aws digitalocean gcp],
+    vagrant: %w[libvirt vbox]
+  }.freeze
 
   # Method analyses the structure of the template
   # @param box_definitions [BoxDefinitions] the provider of box definitions
   # @returns [Symbol] type of the template: vagrant, docker or terraform
   def determine_template_type(box_definitions)
     @node_configurations.map { |_, node| node['box'] }.then do |box_names|
-      box_names.map{ |box_name| box_definitions.get_box(box_name)['provider'] }.uniq
+      box_names.map { |box_name| box_definitions.get_box(box_name)['provider'] }.uniq
     end.then do |providers|
       if providers.size > 1
-        raise("There are several providers defined in the template: #{providers.join(', ')}")
+        raise "There are several providers defined in the template: #{providers.join(', ')}"
       end
+
       providers.first
     end.then do |provider|
       TEMPLATE_TYPE_BY_PROVIDER.each_pair do |template_type, providers|
