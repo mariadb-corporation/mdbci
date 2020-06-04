d5e21cba1fd149460f9ecbadba1040871a847a85
Dmitriy Karpovskiy <dmitriy.karpovskiy@fruct.org>
2020-06-04 18:31:25 +0300

    Use File class to read and write in the config (refs #27118)

--- assets/chef-recipes/cookbooks/mdbe_plugins/recipes/xpand.rb
+++ assets/chef-recipes/cookbooks/mdbe_plugins/recipes/xpand.rb
@@ -8,20 +8,9 @@ when 'rhel', 'centos', 'suse', 'opensuse'
 end
 
 ruby_block 'check configuration' do
-  node.run_state['should_update_config'] = false
   block do
-    cmd = Mixlib::ShellOut.new("cat #{configuration_path}")
-    cmd.run_command
-    config = cmd.stdout
-    node.run_state['should_update_config'] = !node['mdbe_plugins']['xpand_config'].any? do |string|
-      config.include?(string)
+    unless File.read(configuration_path).include?(node['mdbe_plugins']['xpand_config'])
+      File.write(configuration_path, node['mdbe_plugins']['xpand_config'], {mode: 'a:'})
     end
-  end
-end
-
-node['mdbe_plugins']['xpand_config'].each do |str|
-  execute 'add options to xpand config' do
-    command "echo #{str} >> #{configuration_path}"
-    only_if { node.run_state['should_update_config'] }
   end
 end
