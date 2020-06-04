32f1519ae17befac15ae06a7ae44e406c87d9046
Dmitriy Karpovskiy <dmitriy.karpovskiy@fruct.org>
2020-06-04 16:22:50 +0300

    Rename path variable to configuration_path, rename config variable to should_update_config, use any? method for check the config (refs #27118)

--- assets/chef-recipes/cookbooks/mdbe_plugins/recipes/xpand.rb
+++ assets/chef-recipes/cookbooks/mdbe_plugins/recipes/xpand.rb
@@ -1,19 +1,19 @@
 case node[:platform_family]
 when 'debian', 'ubuntu'
   package 'mariadb-plugin-xpand'
-  node.run_state['path'] = '/etc/mysql/mariadb.conf.d/xpand.cnf'
+  configuration_path = '/etc/mysql/mariadb.conf.d/xpand.cnf'
 when 'rhel', 'centos', 'suse', 'opensuse'
   package 'MariaDB-xpand-engine'
-  node.run_state['path'] = '/etc/my.cnf.d/xpand.cnf'
+  configuration_path = '/etc/my.cnf.d/xpand.cnf'
 end
 
 ruby_block 'check configuration' do
-  node.run_state['config'] = false
+  node.run_state['should_update_config'] = false
   block do
-    cmd = Mixlib::ShellOut.new("cat #{node.run_state['path']}")
+    cmd = Mixlib::ShellOut.new("cat #{configuration_path}")
     cmd.run_command
     config = cmd.stdout
-    node.run_state['config'] = !node['mdbe_plugins']['xpand_config'].all? do |string|
+    node.run_state['should_update_config'] = !node['mdbe_plugins']['xpand_config'].any? do |string|
       config.include?(string)
     end
   end
@@ -21,7 +21,7 @@ end
 
 node['mdbe_plugins']['xpand_config'].each do |str|
   execute 'add options to xpand config' do
-    command "echo #{str} >> #{node.run_state['path']}"
-    only_if { node.run_state['config'] }
+    command "echo #{str} >> #{configuration_path}"
+    only_if { node.run_state['should_update_config'] }
   end
 end
