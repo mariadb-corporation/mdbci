ce1d7f83d77cee2aec8d5c2d561e8aedc613c4a1
Dmitriy Karpovskiy <dmitriy.karpovskiy@fruct.org>
2020-07-30 13:01:38 +0300

    Use the File.join method for specifying the path, specify the home directory when creating the user (refs #27389)

--- assets/chef-recipes/cookbooks/user_creation/recipes/default.rb
+++ assets/chef-recipes/cookbooks/user_creation/recipes/default.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
 
+home_dir = File.join('/home', node['user_creation']['name'])
 user node['user_creation']['name'] do
   shell '/bin/bash'
+  home home_dir
 end
 
-directory "/home/#{node['user_creation']['name']}" do
+directory home_dir do
   owner node['user_creation']['name']
 end
 
-file "/etc/sudoers.d/#{node['user_creation']['name']}" do
+file File.join('/etc', 'sudoers.d', node['user_creation']['name']) do
   content "#{node['user_creation']['name']} ALL=(ALL) NOPASSWD: ALL"
 end
 
-directory "/home/#{node['user_creation']['name']}/.ssh" do
+directory File.join(home_dir,  '.ssh') do
   owner node['user_creation']['name']
   action :create
 end
 
 user = ENV['SUDO_USER']
 execute 'copy ssh files' do
-  command "cp -r /home/#{user}/.ssh/ /home/#{node['user_creation']['name']}/"
+  command "cp -r #{File.join('/home', user, '.ssh')} #{home_dir}"
 end
 
 execute 'set rights to ssh files' do
-  command "chown -R #{node['user_creation']['name']} /home/#{node['user_creation']['name']}/.ssh"
+  command "chown -R #{node['user_creation']['name']} #{File.join(home_dir, '.ssh')}"
 end
