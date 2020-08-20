6f09b46028a88ee6130f0d9f02fba97ab1fa7eed
Dmitriy Karpovskiy <dmitriy.karpovskiy@fruct.org>
2020-08-20 10:18:38 +0300

    Add support for new MaxScale repositories (refs #27504)

--- core/commands/generate_repository_partials/maxscale_ci_parser.rb
+++ core/commands/generate_repository_partials/maxscale_ci_parser.rb
@@ -11,14 +11,58 @@ module MaxscaleCiParser
 
     auth = mdbe_ci_config['mdbe_ci_repo']
     releases = []
-    releases.concat(parse_maxscale_ci_rpm_repository(config['repo'], product_version, auth, log, logger))
-    releases.concat(parse_maxscale_ci_deb_repository(config['repo'], product_version, auth, log, logger))
+    releases.concat(parse_maxscale_ci_rpm_repository_new(config['repo'], product_version, auth, log, logger))
+    releases.concat(parse_maxscale_ci_deb_repository_new(config['repo'], product_version, auth, log, logger))
+    releases.concat(parse_maxscale_ci_rpm_repository_old(config['repo'], product_version, auth, log, logger))
+    releases.concat(parse_maxscale_ci_deb_repository_old(config['repo'], product_version, auth, log, logger))
     releases
   end
 
-  def self.parse_maxscale_ci_rpm_repository(config, product_version, auth, log, logger)
+  def self.parse_maxscale_ci_rpm_repository_new(config, product_version, auth, log, logger)
     parse_repository(
-      config['path'], auth, add_auth_to_url(config['key'], auth), 'maxscale_ci', product_version,
+        config['path'], auth, add_auth_to_url(config['new_key'], auth), 'maxscale_ci', product_version,
+        %w[maxscale],
+        ->(url) { url },
+        ->(package, _) { /#{package}/ }, log, logger,
+        save_as_field(:version),
+        append_url(%w[yum]),
+        split_rpm_platforms,
+        extract_field(:platform_version, %r{^(\p{Digit}+)\/?$}),
+        lambda do |release, _|
+          release[:repo] = add_auth_to_url(release[:url], auth)
+          release
+        end
+    )
+  end
+
+  def self.parse_maxscale_ci_deb_repository_new(config, product_version, auth, log, logger)
+    parse_repository(
+        config['path'], auth, add_auth_to_url(config['new_key'], auth), 'maxscale_ci', product_version,
+        %w[maxscale],
+        ->(url) { generate_maxscale_ci_deb_full_url(url) },
+        ->(package, platform) { /#{package}.*#{platform}/ }, log, logger,
+        save_as_field(:version),
+        append_url(%w[apt], nil, true),
+        append_url(%w[dists]),
+        extract_deb_platforms,
+        lambda do |release, _|
+          repo_path = add_auth_to_url(release[:repo_url], auth)
+          release[:repo] = "#{repo_path} #{release[:platform_version]} main"
+          release
+        end
+    )
+  end
+
+  def self.generate_maxscale_ci_deb_full_url(incorrect_url)
+    split_url = incorrect_url.split('/')
+    split_url.pop(2)
+    url = split_url.join('/')
+    "#{url}/pool/main/m/maxscale/"
+  end
+
+  def self.parse_maxscale_ci_rpm_repository_old(config, product_version, auth, log, logger)
+    parse_repository(
+      config['path'], auth, add_auth_to_url(config['old_key'], auth), 'maxscale_ci', product_version,
       %w[maxscale],
       ->(url) { url },
       ->(package, _) { /#{package}/ }, log, logger,
@@ -33,9 +77,9 @@ module MaxscaleCiParser
     )
   end
 
-  def self.parse_maxscale_ci_deb_repository(config, product_version, auth, log, logger)
+  def self.parse_maxscale_ci_deb_repository_old(config, product_version, auth, log, logger)
     parse_repository(
-      config['path'], auth, add_auth_to_url(config['key'], auth), 'maxscale_ci', product_version,
+      config['path'], auth, add_auth_to_url(config['old_key'], auth), 'maxscale_ci', product_version,
       %w[maxscale], ->(url) { "#{url}main/binary-amd64/" },
       ->(package, _) { /#{package}/ }, log, logger,
       save_as_field(:version),
