9bb57e9124b69b84dcea24697eef09973680bb53
Dmitriy Karpovskiy <dmitriy.karpovskiy@fruct.org>
2020-10-14 09:30:29 +0300

    Add x86_64 to the path when generating Galera repo.d for RPM (refs #27756)

--- core/commands/generate_repository_partials/galera_ci_parser.rb
+++ core/commands/generate_repository_partials/galera_ci_parser.rb
@@ -27,6 +27,7 @@ module GaleraCiParser
         append_url(%w[yum]),
         split_rpm_platforms,
         extract_field(:platform_version, %r{^(\p{Digit}+)\/?$}),
+        append_path_if_exists('x86_64'),
         lambda do |release, _|
           release[:repo] = add_auth_to_url(release[:url], auth)
           release
