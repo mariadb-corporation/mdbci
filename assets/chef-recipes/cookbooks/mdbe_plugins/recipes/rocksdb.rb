dcf91e0ff59c1504c1e1f3a92453bc7c479024ed
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2020-10-16 20:18:56 +0300

    Add hack for rocksdb plugin on debian stretch (refs #27785)

--- assets/chef-recipes/cookbooks/mdbe_plugins/recipes/rocksdb.rb
+++ assets/chef-recipes/cookbooks/mdbe_plugins/recipes/rocksdb.rb
@@ -1,6 +1,21 @@
 case node[:platform_family]
 when 'debian', 'ubuntu'
-  package 'mariadb-plugin-rocksdb'
+  # Debian stretch hack
+  if node['platform'] == 'debian' && node['platform_version'].to_i == 9
+    package 'software-properties-common'
+    apt_repository 'name' do
+      components %w[stretch-backports main]
+      uri 'http://ftp.us.debian.org/debian/'
+    end
+    apt_update do
+      action :update
+    end
+    package 'rocksdb-tools' do
+      options '-t stretch-backports'
+    end
+  else
+    package 'mariadb-plugin-rocksdb'
+  end
 when 'rhel', 'centos', 'suse', 'opensuse'
   package 'MariaDB-rocksdb-engine'
 end
