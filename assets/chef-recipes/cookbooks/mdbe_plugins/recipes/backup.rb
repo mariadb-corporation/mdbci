1a3faf87517c193368694df8220c3e28270e8a5c
Dmitriy Karpovskiy <dmitriy.karpovskiy@fruct.org>
2020-11-10 21:50:01 +0300

    Use mariadb-backup-10.2 for MariaDB version 10.2 (refs #27896)

--- assets/chef-recipes/cookbooks/mdbe_plugins/recipes/backup.rb
+++ assets/chef-recipes/cookbooks/mdbe_plugins/recipes/backup.rb
@@ -1,6 +1,21 @@
+require 'mixlib/shellout'
+
+node.run_state[:plugin_name] ='mariadb-backup'
+if node[:platform_family] == 'debian' || node[:platform_family] == 'ubuntu'
+  ruby_block 'get MariaDB version' do
+    block do
+      cmd = Mixlib::ShellOut.new('mysql --version')
+      cmd.run_command
+      node.run_state[:plugin_name] = 'mariadb-backup-10.2' if cmd.stdout.lines[0].chomp.include?('10.2')
+    end
+  end
+end
+
 case node[:platform_family]
 when 'debian', 'ubuntu'
-  package 'mariadb-backup'
+  package 'Install backup package' do
+    package_name(lazy { node.run_state[:plugin_name] } )
+  end
 when 'rhel', 'centos', 'suse', 'opensuse'
   package 'MariaDB-backup'
 end
