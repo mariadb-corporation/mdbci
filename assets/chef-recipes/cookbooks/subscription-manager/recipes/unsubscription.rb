b9a9b14c1839e59a3f16ec9e41b1b6431ac800c5
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2020-11-30 22:36:45 +0300

    Ignore failure on unsubscription step in the subscription-manager recipe (refs #28010)

--- assets/chef-recipes/cookbooks/subscription-manager/recipes/unsubscription.rb
+++ assets/chef-recipes/cookbooks/subscription-manager/recipes/unsubscription.rb
@@ -2,4 +2,6 @@ execute 'Unregistering a system' do
   command 'subscription-manager remove --all && '\
           'subscription-manager unregister && '\
           'subscription-manager clean'
+  ignore_failure
+  returns [0, 1]
 end
