0c717ffdc1596d159343a356ce467c953ec5212d
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2020-11-30 12:35:07 +0300

    Install sssd_krb5 instead of pam_krb5 in the kerberos product recipe on rhel 8 (refs #28004)

--- assets/chef-recipes/cookbooks/kerberos/recipes/default.rb
+++ assets/chef-recipes/cookbooks/kerberos/recipes/default.rb
@@ -1,5 +1,11 @@
 if %w[redhat centos].include?(node[:platform])
-  package %w[krb5-workstation pam_krb5] do
+  packages = %w[krb5-workstation]
+  if node[:platform] == 'redhat' && node[:platform_version].to_i == 8
+    packages << 'sssd'
+  else
+    packages << 'pam_krb5'
+  end
+  package packages do
     flush_cache({ before: true })
   end
 elsif %w[debian ubuntu].include?(node[:platform])
