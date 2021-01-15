d187572fbe7f7f90d3783bdf08b7c060d3696e72
Dmitriy Karpovskiy <dmitriy.karpovskiy@fruct.org>
2021-01-15 13:46:43 +0300

    Move opening cmapi port to the cmapi recipe (refs #28170)

--- assets/chef-recipes/cookbooks/mdbe_plugins/recipes/cmapi.rb
+++ assets/chef-recipes/cookbooks/mdbe_plugins/recipes/cmapi.rb
@@ -1,1 +1,22 @@
 package 'mariadb-columnstore-cmapi'
+
+execute 'Opening cmapi port' do
+  command 'iptables -I INPUT -p tcp -m tcp --dport 8640 -j ACCEPT'
+  command 'iptables -I INPUT -p tcp --dport 8640 -j ACCEPT -m state --state ESTABLISHED,NEW'
+end
+
+case node[:platform_family]
+when 'debian', 'ubuntu'
+  execute 'Save iptables rules' do
+    command 'iptables-save > /etc/iptables/rules.v4'
+  end
+when 'rhel', 'centos', 'suse'
+  bash 'Save iptables rules' do
+    code <<-EOF
+      iptables-save > /etc/sysconfig/iptables
+    EOF
+    timeout 30
+    retries 5
+    retry_delay 30
+  end
+end
