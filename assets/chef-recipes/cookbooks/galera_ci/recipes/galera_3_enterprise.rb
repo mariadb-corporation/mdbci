2aaf62107f0783b81b0b35c02f611ef192984d34
Timofey Turenko <timofey.turenko@mariadb.com>
2021-02-11 00:30:07 +0200

    use galera-enterprise-3 package name for all distros

--- assets/chef-recipes/cookbooks/galera_ci/recipes/galera_3_enterprise.rb
+++ assets/chef-recipes/cookbooks/galera_ci/recipes/galera_3_enterprise.rb
@@ -1,8 +1,3 @@
 include_recipe "galera_ci::galera_repository"
 
-case node[:platform_family]
-when 'debian', 'ubuntu'
-  package 'galera-3'
-else
-  package 'galera'
-end
+package 'galera-enterprise-3'	
