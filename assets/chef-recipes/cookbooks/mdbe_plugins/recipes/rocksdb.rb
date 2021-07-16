72f7d82f669ff893921885a9dadb4bb2e9f0e2d2
Dmitriy Karpovskiy <dmitriy.karpovskiy@fruct.org>
2021-07-16 14:59:19 +0300

    Do not specify the distribution for Debian Stretch in stretch-backports (refs #29032)

--- assets/chef-recipes/cookbooks/mdbe_plugins/recipes/rocksdb.rb
+++ assets/chef-recipes/cookbooks/mdbe_plugins/recipes/rocksdb.rb
@@ -3,9 +3,10 @@ when 'debian', 'ubuntu'
   # Debian stretch hack
   if node['platform'] == 'debian' && node['platform_version'].to_i == 9
     package 'software-properties-common'
-    apt_repository 'name' do
+    apt_repository 'stretch-backports' do
       components %w[stretch-backports main]
       uri 'http://ftp.us.debian.org/debian/'
+      distribution '' if node.attributes['kernel']['machine'] == 'aarch64'
     end
     apt_update do
       action :update
