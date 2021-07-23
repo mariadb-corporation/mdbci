443a741fa5439d5860bcf8d2c8e42a9000653385
Dmitriy Karpovskiy <dmitriy.karpovskiy@fruct.org>
2021-07-23 15:35:23 +0300

    Use de-register only for SLES 15 (refs #29063)

--- assets/chef-recipes/cookbooks/suse-connect/recipes/unsubscription.rb
+++ assets/chef-recipes/cookbooks/suse-connect/recipes/unsubscription.rb
@@ -1,6 +1,11 @@
-execute 'Unregistering a system' do
-  command 'SUSEConnect --de-register'
+if node[:platform_version].to_i == 15
+  execute 'Unregistering a system' do
+    command 'SUSEConnect --de-register'
+  end
 end
 execute 'Clean up a system' do
   command 'SUSEConnect --cleanup'
+end
+execute 'Clean up a system' do
+  command 'registercloudguest --clean'
 end
