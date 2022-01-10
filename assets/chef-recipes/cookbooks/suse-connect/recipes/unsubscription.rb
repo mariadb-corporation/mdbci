1f8cde94cc9d4b3a67f0cd1722e421d75434df14
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2022-01-10 16:07:20 +0300

    Use custom image for Sles SP2 image (refs #29529).
    Ignore failures during the de-configuration of SLES images.

--- assets/chef-recipes/cookbooks/suse-connect/recipes/unsubscription.rb
+++ assets/chef-recipes/cookbooks/suse-connect/recipes/unsubscription.rb
@@ -1,11 +1,14 @@
 if node[:platform_version].to_i == 15
   execute 'Unregistering a system' do
     command 'SUSEConnect --de-register'
+    ignore_failure true
   end
 end
 execute 'Clean up a system' do
   command 'SUSEConnect --cleanup'
+  ignore_failure true
 end
 execute 'Clean up a system' do
   command 'registercloudguest --clean'
+  ignore_failure true
 end
