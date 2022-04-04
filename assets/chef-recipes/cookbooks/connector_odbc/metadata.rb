c184ef3a0f6b27146390b2ce5725877c92e72bc9
Kulakov Daniil <missingtext@yandex.ru>
2022-04-04 18:44:44 +0300

    Add ODBC connector chef recipe (refs #29716)

---
+++ assets/chef-recipes/cookbooks/connector_odbc/metadata.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+name             'connector_odbc'
+maintainer       'Yaroslavl FRUCT Laboratory'
+maintainer_email 'yarfruct@gmail.com'
+license          'All rights reserved'
+description      'Installs MariaDB ODBC connector'
+version          '0.1.0'
---
+++ assets/chef-recipes/cookbooks/connector_odbc/recipes/default.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+include_recipe 'connector_odbc::install_dependencies'
+
+directory '/odbc_package' do
+  action :create
+end
+
+remote_file '/odbc_package/odbc.tar.gz' do
+  source node['connector_odbc']['repo']
+end
+
+execute 'extract_archive' do
+  command 'tar -xvzf /odbc_package/odbc.tar.gz -C /odbc_package/ --strip-components 1'
+end
+
+# Move libmaodbc library to make sure it always will be in the same directory
+execute 'move_libmaodbc' do
+  command 'mv /odbc_package/lib64/mariadb/libmaodbc.so /odbc_package/lib/mariadb'
+  only_if { ::Dir.exist?('/odbc_package/lib64') }
+end
+
+bash 'install_odbc' do
+  user 'root'
+  cwd '/odbc_package'
+  code <<-SCRIPT
+    install lib/mariadb/libmaodbc.so /usr/lib64/
+    install -d /usr/lib64/mariadb/
+    install -d /usr/lib64/mariadb/plugin/
+    install lib/mariadb/plugin/auth_gssapi_client.so /usr/lib64/mariadb/plugin/
+    install lib/mariadb/plugin/caching_sha2_password.so /usr/lib64/mariadb/plugin/
+    install lib/mariadb/plugin/client_ed25519.so /usr/lib64/mariadb/plugin/
+    install lib/mariadb/plugin/dialog.so /usr/lib64/mariadb/plugin/
+    install lib/mariadb/plugin/mysql_clear_password.so /usr/lib64/mariadb/plugin/
+    install lib/mariadb/plugin/sha256_password.so /usr/lib64/mariadb/plugin/
+  SCRIPT
+end
+
+directory '/odbc_package' do
+  action :delete
+  recursive true
+end
