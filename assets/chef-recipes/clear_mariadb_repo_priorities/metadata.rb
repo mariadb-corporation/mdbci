412ec5a2b25fb0e53fd69c9743fe9d51d660e3d4
Kulakov Daniil <missingtext@yandex.ru>
2022-04-07 14:12:38 +0300

    Implement recipe to delete repo priority files left after mdbe_repo_setup script (refs #29799)

---
+++ assets/chef-recipes/clear_mariadb_repo_priorities/metadata.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+name             'connector_odbc'
+maintainer       'Yaroslavl FRUCT Laboratory'
+maintainer_email 'yarfruct@gmail.com'
+license          'All rights reserved'
+description      'Delete repository priorities files left after running mdbe_repo_setup script'
+version          '0.1.0'
