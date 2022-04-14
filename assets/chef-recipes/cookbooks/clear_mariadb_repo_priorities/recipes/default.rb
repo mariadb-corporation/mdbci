f3bacd94b8b606b84e05a79c9edd402cb49422b8
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2022-04-14 16:59:01 +0300

    Remove the Zypper repository during the priority removal procedure (refs #29818)

--- assets/chef-recipes/cookbooks/clear_mariadb_repo_priorities/recipes/default.rb
+++ assets/chef-recipes/cookbooks/clear_mariadb_repo_priorities/recipes/default.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
-filename = '/etc/apt/preferences.d/mariadb-enterprise.pref'
+deb_pin_priority_file = '/etc/apt/preferences.d/mariadb-enterprise.pref'
 
-file filename do
+file deb_pin_priority_file do
   action :delete
-  only_if { ::File.exist?(filename) }
+  only_if { ::File.exist?(deb_pin_priority_file) }
+end
+
+zypper_repo_file = '/etc/zypp/repos.d/mariadb.repo'
+
+file zypper_repo_file do
+  action :delete
+  only_if { ::File.exist?(zypper_repo_file) }
 end
