6546aff7dbf5fbae5eb1b2760367499c2c9ae675
Kulakov Daniil <missingtext@yandex.ru>
2022-04-22 13:51:05 +0300

    Fix incorrect MariaDB plugins installing (refs #29799)

---
+++ assets/chef-recipes/cookbooks/galera_ci/metadata.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+name             'galera_ci'
+maintainer       'Yaroslavl FRUCT Laboratory'
+maintainer_email 'yarfruct@gmail.com'
+license          'All rights reserved'
+description      'Configures Galera from CI repositories'
+version          '0.1.0'
+
+depends          'clear_mariadb_repo_priorities'
