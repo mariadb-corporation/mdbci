13ce44692e8fb568c670d621640edd00c7788731
Kulakov Daniil <missingtext@yandex.ru>
2022-04-22 13:51:05 +0300

    Fix incorrect MariaDB plugins installing (refs #29799)

---
+++ assets/chef-recipes/cookbooks/mdbe_plugins/metadata.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+name             'mdbe_plugins'
+maintainer       'Yaroslavl FRUCT Laboratory'
+maintainer_email 'yarfruct@gmail.com'
+license          'All rights reserved'
+description      'Configures MariaDB plugins'
+version          '0.1.0'
+
+depends          'clear_mariadb_repo_priorities'
