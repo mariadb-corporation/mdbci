17828bc851cc25895f705a38a47b0dab83f16b4c
Kulakov Daniil <missingtext@yandex.ru>
2022-04-27 17:09:24 +0300

    Add clarification (refs #29854)

--- core/services/machine_configurator.rb
+++ core/services/machine_configurator.rb
@@ -4,6 +4,19 @@ require 'io/console'
 require_relative '../models/result'
 require_relative '../services/log_storage'
 
+# This file is part of MDBCI.
+#
+# MDBCI is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# MDBCI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with Foobar.
+# If not, see <https://www.gnu.org/licenses/>.
+
 # Class allows to configure a specified machine using the chef-solo,
 # MDBCI coockbooks and roles.
 class MachineConfigurator
