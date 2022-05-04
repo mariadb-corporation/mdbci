b5c2579221252b9ebf5e5353cbcc493cbf2800bc
Kulakov Daniil <missingtext@yandex.ru>
2022-05-04 23:23:24 +0300

    Removed logger from ssh_commands module, refactor code (refs #29854)

--- core/services/ssh_commands.rb
+++ core/services/ssh_commands.rb
@@ -22,16 +22,11 @@ module SshCommands
   # Run command on the remote machine, using ssh command on the local machine
   # @param machine [Hash] information about machine to connect
   # @param command [String] command to run
-  # @param logger [Out] logger to log information into
   # @return result of the command execution with stdin or stderr output
-  def self.execute_ssh(machine, command, logger)
-    logger.info("Running '#{command}' on the #{machine['network']} machine")
+  def self.execute_ssh(machine, command)
     ssh_login = "#{machine['whoami']}@#{machine['network']}"
     command = "ssh #{ARGUMENTS} -i '#{machine['keyfile']}' '#{ssh_login}' '#{command}'"
-    parse_result(
-      ShellCommands.run_command_with_stderr(command, logger, false),
-      logger
-    )
+    execute_command(command)
   end
 
   # Uploads specified file to the remote machine using scp command on the local machine
@@ -39,54 +34,34 @@ module SshCommands
   # @param source [String] path to the file on the local machine
   # @param target [String] path to the file on the remote machine
   # @param recursive [Boolean] use recursive copying or not
-  # @param logger [Out] logger to log information into
   # @return result of the command execution with stdout or stderr output
-  def self.execute_scp(machine, source, target, logger, recursive = true)
-    logger.info("Copying files via scp on the #{machine['network']} machine")
+  def self.execute_scp(machine, source, target, recursive = true)
     target_dir = File.dirname(target)
-    execute_ssh(machine, "mkdir -p #{target_dir}", logger)
+    execute_ssh(machine, "mkdir -p #{target_dir}")
     ssh_login = "#{machine['whoami']}@#{machine['network']}"
     command = "scp #{ARGUMENTS} -i '#{machine['keyfile']}' #{recursive ? ' -r ' : ''} '#{source}' '#{ssh_login}:#{target}'"
-    parse_result(
-      ShellCommands.run_command_with_stderr(command, logger, false),
-      logger
-    )
+    execute_command(command)
   end
 
   # Generate ssh keys on the local machine using ssh-keygen command
   # @param filepath [String] absolute path to the file to write ssh-keys into.
   #   Private key will be created with the .pem extension
   # @param user [String] user name to whom the keys will be created
-  # @param logger [Out] logger to log information into
   # @return result of the command execution with stdout or stderr output
-  def self.execute_ssh_keygen(filepath, user, logger)
-    logger.info('Generating ssh keys')
+  def self.execute_ssh_keygen(filepath, user)
     command = "ssh-keygen -q -t rsa -f '#{filepath}' -C '#{user}' -b 2048 -N ''"
-    parse_result(
-      ShellCommands.run_command_with_stderr(command, logger, false),
-      logger
-    )
+    execute_command(command)
   end
 
-  # Parses command exit status, logs stdout and stderr and returns corresponding results
-  def self.parse_result(command_results, logger)
-    if command_results[:value].exitstatus.zero?
-      converted_data = command_results[:stdout].force_encoding('UTF-8')
-      log_printable_lines(converted_data, logger)
+  # Executes command, returns corresponding results with stdout/stderr output
+  def self.execute_command(command)
+    result = ShellCommands.run_command_with_stderr(command)
+    if result[:value].exitstatus.zero?
+      converted_data = result[:stdout].force_encoding('UTF-8')
       Result.ok(converted_data)
     else
-      converted_data = "#{command_results[:stdout]}\n#{command_results[:stderr]}".force_encoding('UTF-8')
-      log_printable_lines(converted_data, logger)
+      converted_data = result[:stderr].force_encoding('UTF-8')
       Result.error(converted_data)
-    end
-  end
-
-  # Log command output in human-readable format
-  def self.log_printable_lines(lines, logger)
-    lines.split("\n").map(&:chomp)
-         .grep(/\p{Graph}+/mu)
-         .each do |line|
-      logger.debug("ssh: #{line}")
     end
   end
 end
