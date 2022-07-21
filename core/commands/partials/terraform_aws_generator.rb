6cec5dd878f41de786f4a2555d5198e633becc59
r.mamaev <mamaew.roma2000@yandex.ru>
2022-07-21 12:52:51 +0300

    Add delete_on_termination = true option (refs #30129)

--- core/commands/partials/terraform_aws_generator.rb
+++ core/commands/partials/terraform_aws_generator.rb
@@ -285,6 +285,7 @@ class TerraformAwsGenerator
         size = 100
       }
       resource "aws_volume_attachment" "<%= name %>-disk-attachment" {
+        delete_on_termination = true
         device_name = "/dev/sdh"
         volume_id   = aws_ebs_volume.<%= name %>-disk.id
         instance_id = aws_instance.<%= name %>.id
