44c56ee254dfcb5b2f988c163cf028b2a97403af
Grigoriy Kostryukov <kostryukov-grigoriy@mail.ru>
2023-01-12 15:14:04 +0300

    Make Java-11 default version for CentOS and RHEL 7 (refs #30875)

--- assets/chef-recipes/cookbooks/java/recipes/default.rb
+++ assets/chef-recipes/cookbooks/java/recipes/default.rb
@@ -1,6 +1,10 @@
 java_version = node['java']['version'].nil? ? 'latest' : node['java']['version']
 platform_version = node[:platform_version].to_i
 
+if java_version == 'latest' && platform_version == 7
+  java_version = 11
+end
+
 if [8, 9].include?(platform_version)
     execute 'install epel-release' do
         command "yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-#{platform_version}.noarch.rpm"
