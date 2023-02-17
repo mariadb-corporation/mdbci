e124d6e5f3ce650013002618dcd9415da82c2033
Grigoriy Kostryukov <kostryukov-grigoriy@mail.ru>
2023-02-17 14:59:41 +0300

    Remove unused cleanup subscription parameters (refs #30893)

--- core/commands/destroy_command.rb
+++ core/commands/destroy_command.rb
@@ -171,7 +171,7 @@ Labels should be separated with commas, do not contain any whitespaces.
         uninstall_products(configuration, network_settings_result.value, registry_result.value)
       end
     end
-    cleanup_result = cleanup_leftover_subscriptions(configuration.provider, configuration_path)
+    cleanup_result = cleanup_leftover_subscriptions(configuration_path)
     @ui.error(cleanup_result.error) if cleanup_result.error?
     if configuration.docker_configuration?
       docker_cleaner = DockerSwarmCleaner.new(@env, @ui)
@@ -200,7 +200,7 @@ Labels should be separated with commas, do not contain any whitespaces.
   end
 
   # Withdraw all subscriptions that were not removed by Chef
-  def cleanup_leftover_subscriptions(provider, configuration_path)
+  def cleanup_leftover_subscriptions(configuration_path)
     @ui.info('Cleaning up leftover systems subscriptions')
     registration_manager = RegistrationManager.new(@env.suse_config['registration_proxy_url'], configuration_path, @ui)
     registration_manager.cleanup_subscriptions
