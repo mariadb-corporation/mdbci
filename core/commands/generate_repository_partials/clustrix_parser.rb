b0d6ef15d987589c2ee8c2eb3cfa6448b6370d35
Danil Nazarov <nazarovdo@hotmail.com>
2023-08-11 15:55:38 +0300

    Add supported platforms list generator to Clustrix parser (refs #31439)

--- core/commands/generate_repository_partials/clustrix_parser.rb
+++ core/commands/generate_repository_partials/clustrix_parser.rb
@@ -68,6 +68,23 @@ module ClustrixParser
     link.match(/el\d+.tar.bz/i).to_s.split(/[^\d]/).join
   end
 
+  def self.get_supported_platforms(config, private_key, link_name)
+    supported_platforms = []
+    paths = [setup_private_key(config['path'], private_key)]
+    path_uri = URI.parse(paths.first)
+    2.times do
+      paths = get_release_paths(path_uri, paths, link_name)
+    end
+    paths.map do |path|
+      get_clustrix_tar_links(path)
+    end.flatten.map do |link|
+      tar_url = generate_url_by_link(path_uri, link)
+      platform_version = get_platform_version_by_link(tar_url).to_s
+      supported_platforms += ["centos_#{platform_version}", "rhel_#{platform_version}"]
+    end
+    supported_platforms.uniq
+  end
+
   def self.generate_clustrix_release_info(platforms, release_info, product_name)
     platforms.map do |platform_and_version|
       platform, platform_version = platform_and_version.split('_')
@@ -78,8 +95,9 @@ module ClustrixParser
   end
 
   def self.parse_clustrix_repository(config, private_key, link_name, product_name)
+    supported_platforms = get_supported_platforms(config, private_key, link_name)
     get_clustrix_release_versions(config, private_key, link_name).map do |release_info|
-      generate_clustrix_release_info(config['platforms'], release_info, product_name).uniq
+      generate_clustrix_release_info(supported_platforms, release_info, product_name).uniq
     end.flatten
   end
 end
