e2ef90f90fe09a928d4eee2fee22b378e83a297f
Danil Nazarov <nazarovdo@hotmail.com>
2023-09-12 19:43:53 +0300

    Add Kafka Chef recipe (refs #31429)

--- assets/chef-recipes/cookbooks/kafka/recipes/default.rb
+++ assets/chef-recipes/cookbooks/kafka/recipes/default.rb
@@ -1,1 +1,24 @@
 # frozen_string_literal: true
+
+user = ENV['SUDO_USER']
+home_dir = Dir.home(user)
+
+remote_file File.join(home_dir, 'kafka.tgz') do
+  source node['kafka']['repo']
+  action :create
+end
+
+#execute 'Untar Kafka archive' do
+#  command "tar zxf #{File.join(home_dir, 'kafka.tgz')}"
+#end
+
+directory File.join(home_dir, 'kafka')
+
+execute 'Untar Kafka archive' do
+  command "tar xf #{File.join(home_dir, 'kafka.tgz')}"\
+          " -C #{File.join(home_dir, 'kafka')} --strip-components=1"
+end
+
+file File.join(home_dir, 'kafka.tgz') do
+  action :delete
+end
