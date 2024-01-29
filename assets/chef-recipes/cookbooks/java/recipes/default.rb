716c788361d355db03a2187310a8ba78421ce35b
Danil Nazarov <danil.nazarov@fruct.org>
2024-01-29 21:01:41 +0300

    Add Debian/Ubuntu support for Java product

--- assets/chef-recipes/cookbooks/java/recipes/default.rb
+++ assets/chef-recipes/cookbooks/java/recipes/default.rb
@@ -1,16 +1,32 @@
 java_version = node['java']['version'].nil? ? 'latest' : node['java']['version']
 platform_version = node[:platform_version].to_i
+platform = node[:platform]
 
-if java_version == 'latest' && platform_version == 7
-  java_version = 11
-end
+if ['rhel', 'rocky', 'centos'].include?(platform)
+    if java_version == 'latest' && platform_version == 7
+        java_version = 11
+    end
 
-if [8, 9].include?(platform_version)
-    execute 'install epel-release' do
-        command "yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-#{platform_version}.noarch.rpm"
+    if [8, 9].include?(platform_version)
+        execute 'install epel-release' do
+            command "yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-#{platform_version}.noarch.rpm"
+        end
+    end
+
+    package "java-#{java_version}-openjdk" do
+        flush_cache({ before: true })
     end
 end
 
-package "java-#{java_version}-openjdk" do
-    flush_cache({ before: true })
+if ['debian', 'ubuntu'].include?(platform)
+    apt_update
+    if java_version == 'latest'
+        apt_package 'default-jdk' do
+            action :install
+        end
+    else
+        apt_package "openjdk-#{java-version}-jdk" do
+            action :install
+        end
+    end
 end
