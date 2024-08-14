ce4684a1550ba7a5a1b8ec74f070b9883c7e7654
Danil Nazarov <danil.nazarov@fruct.org>
2024-08-14 18:44:05 +0300

    Add fix dependencies key to build-dep command

--- assets/chef-recipes/cookbooks/mdbe_build/recipes/default.rb
+++ assets/chef-recipes/cookbooks/mdbe_build/recipes/default.rb
@@ -371,7 +371,7 @@ when 'debian'
     action :update
   end
   execute 'install dependencies mariadb-server' do
-    command "apt-get --yes build-dep --quiet mariadb-server"
+    command "apt-get -f --yes build-dep --quiet mariadb-server --target-release #{node.attributes['lsb']['codename']}"
   end
 when 'ubuntu'
   case node[:platform_version].to_f
@@ -413,7 +413,7 @@ when 'ubuntu'
     action :update
   end
     execute 'install dependencies mariadb-server' do
-      command "apt-get --yes build-dep --quiet mariadb-server"
+      command "apt-get -f --yes build-dep --quiet mariadb-server --target-release #{node.attributes['lsb']['codename']}"
     end
 when 'centos', 'redhat', 'rocky', 'almalinux'
   case node[:platform_version].to_i
