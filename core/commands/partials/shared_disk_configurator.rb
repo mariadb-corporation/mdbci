327c219d79df65179439cba97b328ccddb5e8072
Danil Nazarov <danil.nazarov@fruct.org>
2024-09-11 18:54:57 +0300

    Set reading permissions for libvirt shared disk image files and directories (FRUC-172)

--- core/commands/partials/shared_disk_configurator.rb
+++ core/commands/partials/shared_disk_configurator.rb
@@ -16,6 +16,8 @@ class SharedDiskConfigurator
   def create_libvirt_disk_images(shared_disks)
     image_dir_path = "#{@configuration_path}/images"
     FileUtils.mkdir_p(image_dir_path)
+    FileUtils.chmod 0775, image_dir_path, :verbose => true
+    FileUtils.chmod 0771, @configuration_path, :verbose => true
     shared_disks.each do |disk|
       disk_id = disk[0]
       disk_size = disk[1]['size']
@@ -25,6 +27,7 @@ class SharedDiskConfigurator
         "qemu-img create -f raw #{disk_id}.img #{disk_size}",
         image_dir_path
       )
+      FileUtils.chmod 0771, "#{image_dir_path}/#{disk_id}.img", :verbose => true
       unless command[:value].success?
         @ui.error("Failed to create QEMU/KVM disk image: #{disk_id}")
         return Result.error("Failed to create QEMU/KVM disk image: #{disk_id}")
