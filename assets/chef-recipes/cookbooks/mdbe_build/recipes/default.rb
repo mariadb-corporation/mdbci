38973044122f55d63d1889b72d93ab6965724fc6
Danil Nazarov <danil.nazarov@fruct.org>
2024-10-25 12:32:54 +0300

    Build dependencies for all installation scenarios in mdbe_build recipe (refs FRUC-184)

--- assets/chef-recipes/cookbooks/mdbe_build/recipes/default.rb
+++ assets/chef-recipes/cookbooks/mdbe_build/recipes/default.rb
@@ -407,9 +407,11 @@ when 'debian'
     apt_update 'update apt cache' do
       action :update
     end
-    execute 'install dependencies mariadb-server' do
-      command "apt-get --fix-broken --yes build-dep --quiet --allow-downgrades mariadb-server --target-release #{node.attributes['lsb']['codename']}"
-    end
+  end
+  execute 'install dependencies mariadb-server' do
+    command "apt-get --fix-broken --yes build-dep --quiet --allow-downgrades mariadb-server"
+  end
+  if File.file?('/etc/apt/sources.list.d/mariadb.list.save')
     execute 'enable MariaDB repo' do
       command "sudo mv /etc/apt/sources.list.d/mariadb.list.save /etc/apt/sources.list.d/mariadb.list"
     end
@@ -468,9 +470,11 @@ when 'ubuntu'
     apt_update 'update apt cache' do
       action :update
     end
-    execute 'install dependencies mariadb-server' do
-      command "apt-get --fix-broken --yes build-dep --quiet --allow-downgrades mariadb-server --target-release #{node.attributes['lsb']['codename']}"
-    end
+  end
+  execute 'install dependencies mariadb-server' do
+    command "apt-get --fix-broken --yes build-dep --quiet --allow-downgrades mariadb-server"
+  end
+  if File.file?('/etc/apt/sources.list.d/mariadb.list.save')
     execute 'enable MariaDB repo' do
       command "sudo mv /etc/apt/sources.list.d/mariadb.list.save /etc/apt/sources.list.d/mariadb.list"
     end
