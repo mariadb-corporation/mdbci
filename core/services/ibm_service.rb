3b8dfcc08fa55c514b28cfcd784b78d35655df55
Danil Nazarov <danil.nazarov@fruct.org>
2025-01-23 02:46:26 +0300

    Add API operations to IBM service

--- core/services/ibm_service.rb
+++ core/services/ibm_service.rb
@@ -1,54 +1,62 @@
 # frozen_string_literal: true
+require 'json'
+require 'net/http'
 
 # This class allows to execute commands in accordance to the IBM Cloud provider
 class IbmService
 
     def initialize(ibm_config, logger)
       @logger = logger
-
       if ibm_config.nil?
         @configured = false
         return
       end
-  
       @ibm_config = ibm_config
       @configured = true
-      @cloud_instance_id = @ibm_config['workspace_id']
-      @cloud_connection_id = 'cloud_connection_id_example' # String | Cloud Connection ID
+      @cloud_instance_id = ibm_config['workspace_id']
+      @ibm_crn = ibm_config['crn']
+      @ibm_api_key = ibm_config['api_key']
+      @ibm_tenant_id = ibm_config['tenant_id']
+      @ibm_region = ibm_config['region']
+      @ibm_iam_token = retrieve_iam_token
     end
   
-    def iam_token
-      command = "curl -k -X POST \
-      --header \"Content-Type: application/x-www-form-urlencoded\" \
-      --header \"Accept: application/json\" \
-      --data-urlencode \"grant_type=urn:ibm:params:oauth:grant-type:apikey\" \
-      --data-urlencode \"apikey=#{@ibm_config['api_key']}\" \
-      \"https://iam.cloud.ibm.com/identity/token\"  |jq -r '(.token_type + " " + .access_token)'"
-      ShellCommands.run_command_with_stderr(command)
-      token = command[:stdout]
-      return token
+    def retrieve_iam_token
+      uri = URI("https://iam.cloud.ibm.com/identity/token")
+      req = Net::HTTP::Post.new(uri)
+      req["Accept"] = "application/json"
+      req["Content-Type"] = "application/x-www-form-urlencoded"
+      req.set_form_data('grant_type' => 'urn:ibm:params:oauth:grant-type:apikey', 'apikey' => @ibm_api_key)
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
+        http.request(req)
+      }
+      token = JSON.parse(res.body)
+      "#{token['token_type']} #{token['access_token']}"
     end
 
-    # Fetch machines types list for the given zone.
-    # @return [Array<Hash>] instance types in format { ram, cpu, type }.
-    def machine_types_list(zone)
-  
+    def send_delete_request(uri)
+      req = Net::HTTP::Delete.new(uri)
+      req["Authorization"] = @ibm_iam_token
+      req["CRN"] = @ibm_crn
+      req["Content-Type"] = "application/json"
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
+        http.request(req)
+      }
+      res.body
     end
 
-    # Delete instance specified by its name
-    # @param instance_name [String] name of the instance to delete.
-    def delete_instance(instance_name)
-      cloud_instance_id = @cloud_instance_id
+    def delete_instance_key(public_network_name)
+      uri = URI("https://#{@ibm_region}.power-iaas.cloud.ibm.com/pcloud/v1/tenants/#{@ibm_tenant_id}/sshkeys/#{public_network_name}")
+      send_delete_request(uri)
     end
 
-    def delete_volume()
-      command = "curl -X DELETE https://us-east.power-iaas.cloud.ibm.com/pcloud/v1/cloud-instances/#{@ibm_config['workspace_id']}/volumes/#{volume_id} -H 'Authorization: Bearer <>' -H 'CRN: crn:v1...' -H 'Content-Type: application/json'"
-      ShellCommands.run_command_with_stderr(command)
-      token = command[:stdout]
-      return token
+    def delete_instance_public_network(public_network_id)
+      uri = URI("https://#{@ibm_region}.power-iaas.cloud.ibm.com/pcloud/v1/cloud-instances/#{@cloud_instance_id}/networks/#{public_network_id}")
+      send_delete_request(uri)
     end
 
-    def destroy_instance()
+    def delete_instance(pvm_instance_id)
+      uri = URI("https://#{@ibm_region}.power-iaas.cloud.ibm.com/pcloud/v1/cloud-instances/#{@cloud_instance_id}/pvm-instances/#{pvm_instance_id}")
+      send_delete_request(uri)
     end
   end
-  
