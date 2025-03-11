281874962a3a26a1b5a9fc21950a8b53d4bf3030
Mihail Savostikov <mihail.savostikov@fruct.org>
2025-03-11 15:44:34 +0300

    Formatting and deleting the default box name (resolves #33496)

--- core/commands/create_box_command.rb
+++ core/commands/create_box_command.rb
@@ -1,122 +1,126 @@
-
 require_relative 'base_command'
-require_relative 'partials/vagrant_box_manager'
 require_relative '../models/result'
 require_relative '../models/configuration'
 require_relative '../services/created_box_data_manager'
 require_relative '../services/box_definitions'
+require_relative '../services/vagrant_service'
 
 # The command create new vagrant box .
 class CreateBoxCommand < BaseCommand
-  
-  
-    # rubocop:disable Metrics/MethodLength
-    def show_help
-      info = <<-HELP
+  # rubocop:disable Metrics/MethodLength
+  def show_help
+    info = <<-HELP
 
 
-"create-box" creates a Vagrant Box based on the template machine. 
+"create-box" creates a Vagrant Box based on the template machine.#{' '}
 
 OPTIONS:
 --template:
   Uses [configuration file] for running instance. By default instance.json will be used as configuration template.
 --box-name:
-  Uses [box name] for creating the name of the new Vagrant Box. By default, new boxes are called new-box
+  Uses [box name] for creating the name of the new Vagrant Box.
 If any of the labels passed to the command match any label in the machine description,
 then this machine will be brought up and configured according to its configuration.
 Labels should be separated with commas and should not contain any whitespaces.
 
-      HELP
-      @ui.info(info)
-    end
+    HELP
+    @ui.info(info)
+  end
 
-    CONFIG_DIR = "conf"
-  
-    #Сalls the command generate
-    def generate_command
-      command = GenerateCommand.new([CONFIG_DIR], @env, @ui)
-      command.execute
-    end
+  CONFIG_DIR = "conf"
 
-    #Сalls the command up
-    def up_command
-      command = UpCommand.new([CONFIG_DIR], @env, @ui)
-      command.execute
-    end
+  # Сalls the command generate
+  def generate_command
+    command = GenerateCommand.new([CONFIG_DIR], @env, @ui)
+    command.execute
+  end
 
-    #Сalls the command destroy
-    def destroy_command
-      command = DestroyCommand.new([CONFIG_DIR], @env, @ui)
-      command.execute
-    end
+  # Сalls the command up
+  def up_command
+    command = UpCommand.new([CONFIG_DIR], @env, @ui)
+    command.execute
+  end
 
-    #Create Vagrant box and adds it to Vagrant
-    def create_box
-      node_name = @config.node_names[0]
-      parent_box_name = @config.node_configurations[node_name]["box"]
-      box_param = @boxes.get_box(parent_box_name)
-      products = @config.node_configurations[node_name]["products"]
-      time_start_create = Time.now.strftime("%Y-%m-%d--%H:%M:%S")
-      @created_box_data_manager.generate_info_for_vagrant(time_start_create, parent_box_name, box_param, products, CONFIG_DIR)
+  # Сalls the command destroy
+  def destroy_command
+    command = DestroyCommand.new([CONFIG_DIR], @env, @ui)
+    command.execute
+  end
 
-      @box_manager.create_box(node_name, @env.boxName, time_start_create)
+  # Create Vagrant box and adds it to Vagrant
+  def create_box
+    node_name = @config.node_names[0]
+    parent_box_name = @config.node_configurations[node_name]["box"]
+    parent_box_param = @boxes.get_box(parent_box_name)
+    products = @config.node_configurations[node_name]["products"]
+    time_start_create = Time.now.strftime("%Y-%m-%d--%H:%M:%S")
+    @created_box_data_manager.generate_info_for_vagrant(time_start_create, parent_box_name,
+                                                        parent_box_param, products, CONFIG_DIR)
 
-      @created_box_data_manager.generate_box_info(parent_box_name, @env.boxName, time_start_create, @boxes)
-    end
+    VagrantService.package(node_name, @env.boxName, @ui, @config.path)
+    VagrantService.box_add(time_start_create, @env.boxName, @ui, @config.path)
 
-    def destroy_old_box
-      if @boxes.box_exists?(@env.boxName)
-        @box_manager.destroy_box(@boxes.get_box(@env.boxName)["box"])
-        @created_box_data_manager.delete_box(@env.boxName)
-      end
-    end
+    @created_box_data_manager.generate_box_info(parent_box_name, @env.boxName, time_start_create,
+                                                @boxes)
+  end
 
-    def read_template_type
-      template_file = File.expand_path(@env.template_file)
-      ConfigurationTemplate.from_path(template_file).and_then do |template|
-        ConfigurationTemplate.determine_template_type(template, @env.box_definitions)
-      end.and_then do |template_type|
-        @template_type = template_type
-        Result.ok('Template read')
-      end
-    end
-
-    def execute
-      if @env.show_help
-        show_help
-        puts "#{}"
-        return SUCCESS_RESULT
-      end
-
-      @boxes = @env.box_definitions
-      @created_box_data_manager = CreatedBoxDataManager.new(@ui, @env)
-      if @boxes.box_exists?(@env.boxName) && !@created_box_data_manager.box_exists?(@env.boxName)
-        return Result.error('Wrong box name')
-      end
-
-      read_template_type
-      if @template_type != :vagrant
-        return Result.error('Wrong configuration type')
-      end 
-
-      exit_code = generate_command
-      return exit_code unless exit_code.success? 
-
-      @config = Configuration.new(CONFIG_DIR, @env.labels)
-      if @config.node_names.size != 1  
-        exit_code = destroy_command
-        return exit_code unless exit_code.success?
-        return Result.error('Incorrect number of nodes in the configuration')
-      end  
-
-      @box_manager = VagrantBoxManager.new(@env, @ui, @config)
-      destroy_old_box
-
-      exit_code = up_command
-      return exit_code unless exit_code.success?
-
-      create_box
-
-      destroy_command
+  def destroy_old_box
+    if @boxes.box_exists?(@env.boxName)
+      VagrantService.box_remove(@env.boxName, @ui, @config.path)
+      @created_box_data_manager.delete_box(@env.boxName)
     end
   end
+
+  def read_template_type
+    template_file = File.expand_path(@env.template_file)
+    ConfigurationTemplate.from_path(template_file).and_then do |template|
+      ConfigurationTemplate.determine_template_type(template, @env.box_definitions)
+    end.and_then do |template_type|
+      @template_type = template_type
+      Result.ok('Template read')
+    end
+  end
+
+  def execute
+    if @env.show_help
+      show_help
+      puts "#{}"
+      return SUCCESS_RESULT
+    end
+
+    if @env.boxName.nil?
+      return Result.error('Empty box name')
+    end
+
+    @boxes = @env.box_definitions
+    @created_box_data_manager = CreatedBoxDataManager.new(@env.configuration_path)
+    if @boxes.box_exists?(@env.boxName) && !@created_box_data_manager.box_exists?(@env.boxName)
+      return Result.error('Wrong box name')
+    end
+
+    read_template_type
+    if @template_type != :vagrant
+      return Result.error('Wrong configuration type')
+    end
+
+    exit_code = generate_command
+    return exit_code unless exit_code.success?
+
+    @config = Configuration.new(CONFIG_DIR, @env.labels)
+    if @config.node_names.size != 1
+      exit_code = destroy_command
+      return exit_code unless exit_code.success?
+
+      return Result.error('Incorrect number of nodes in the configuration')
+    end
+
+    destroy_old_box
+
+    exit_code = up_command
+    return exit_code unless exit_code.success?
+
+    create_box
+
+    destroy_command
+  end
+end
--- core/services/created_box_data_manager.rb
+++ core/services/created_box_data_manager.rb
@@ -6,26 +6,25 @@
 require_relative '../out'
 require_relative '../models/result'
 
-#This is a class for managing the data of created boxes
+# This is a class for managing the data of created boxes
 class CreatedBoxDataManager
-
-  def initialize(log, env)
-    @ui = log
-    env = env.clone
-    dir_path = File.join(env.configuration_path, "boxes")
+  def initialize(configuration_path)
+    dir_path = File.join(configuration_path, "boxes")
     @json_path = File.join(dir_path, "created-by-create-box-command.json")
     FileUtils.mkpath(dir_path)
     if File.size?(@json_path).nil?
-      File.open(@json_path,"w") do |file|
+      File.open(@json_path, "w") do |file|
         file.puts "{"
         file.puts "}"
       end
     end
-    @crafted_boxes_inf = JSON.parse(File.read(@json_path))
+    @crafted_boxes_information = JSON.parse(File.read(@json_path))
   end
 
-  def generate_info_for_vagrant(start_time, parent_box_name, box_param, products, path_to_node)
-    inf = {Time: start_time.gsub('--', ' '), Parent_box: parent_box_name, Provider: box_param["provider"], Products: products}
+  def generate_info_for_vagrant(start_time, parent_box_name, parent_box_parameters, products,
+                                path_to_node)
+    inf = { Time: start_time.gsub('--', ' '), Parent_box: parent_box_name,
+            Provider: parent_box_parameters["provider"], Products: products }
     File.open(File.join(path_to_node, "info.json"), "w") do |file|
       file.puts JSON.generate(inf)
     end
@@ -33,21 +32,19 @@ class CreatedBoxDataManager
 
   def delete_box(box_name)
     check_box(box_name)
-    @crafted_boxes_inf.delete(box_name)
+    @crafted_boxes_information.delete(box_name)
   end
 
   def box_exists?(box_name)
-    @crafted_boxes_inf.key?(box_name)
+    @crafted_boxes_information.key?(box_name)
   end
 
   def check_box(box_name)
-    raise ArgumentError, "The specified box definition can not be found: #{box_name}" unless @crafted_boxes_inf.key?(box_name)
+    raise ArgumentError,
+          "The specified box definition can not be found: #{box_name}" unless @crafted_boxes_information.key?(box_name)
   end
 
   def generate_box_info(parent_box_name, new_box_name, start_time, boxes)
-    if new_box_name.nil?
-      new_box_name = "new-box"
-    end
     opts = {
       array_nl: "\n",
       object_nl: "\n",
@@ -56,17 +53,16 @@ class CreatedBoxDataManager
       space: ' '
     }
 
-    box_param = boxes.get_box(parent_box_name)
-    box_param["box"] = "#{new_box_name}--#{start_time}"
-    if box_param.key?("box_version")
-      box_param["box_version"] = "0"
+    box_parameters = boxes.get_box(parent_box_name)
+    box_parameters["box"] = "#{new_box_name}--#{start_time}"
+    if box_parameters.key?("box_version")
+      box_parameters["box_version"] = "0"
     end
 
-    box_rec = {"#{new_box_name}": box_param}
+    box_record = { "#{new_box_name}": box_parameters }
 
-    File.open(@json_path,"w") do |file|
-      file.puts JSON.generate(@crafted_boxes_inf.merge(box_rec), opts)
-    end 
+    File.open(@json_path, "w") do |file|
+      file.puts JSON.generate(@crafted_boxes_information.merge(box_record), opts)
+    end
   end
-
 end
