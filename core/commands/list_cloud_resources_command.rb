ada43c650b61315437f19f32e6cff949fbf93163
Danil Nazarov <danil.nazarov@fruct.org>
2025-05-06 11:22:20 +0300

    Correct --all-regions key description typo (refs FRUC-238)

--- core/commands/list_cloud_resources_command.rb
+++ core/commands/list_cloud_resources_command.rb
@@ -20,7 +20,7 @@ The command shows a list of active resources: instances, disks (volumes), securi
 Add the --json flag to show the machine readable text.
 Add the --hours NUMBER_OF_HOURS flag to display the resources older than this hours.
 Add the --output-file FILENAME flag to generate a report as a JSON with the specified name.
-Add the --all-regions flag to show instances in all of the available regions (AWS only).
+Add the --all-regions flag to show resources in all of the available regions (AWS only).
 
 If --hours flag is not specified, all runnung resources will be shown.
 The command ends with an error if any resource is present, no otherwise
