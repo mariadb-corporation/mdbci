ce55d975a8ffeeb359de4198377f74c86d4b65b6
Danil Nazarov <danil.nazarov@fruct.org>
2025-09-30 16:10:56 +0300

    Add MemaAgent recipe (refs FRUC-282)

---
+++ assets/chef-recipes/cookbooks/mema_agent/recipes/mema_agent_repos.rb
@@ -0,0 +1,26 @@
+case node[:platform_family]
+when "debian", "ubuntu"
+  apt_repository 'mema_agent' do
+    key node['mema_agent']['repo_key']
+    uri node['mema_agent']['repo']
+    components node['mema_agent']['components']
+    sensitive true
+  end
+when "rhel", "centos", "almalinux"
+  yum_repository 'mema_agent' do
+    baseurl node['mema_agent']['repo']
+    gpgkey node['mema_agent']['repo_key']
+    gpgcheck true
+    sensitive true
+  end
+when "suse", "opensuse", "sles"
+  zypper_repository 'mema_agent' do
+    baseurl node['mema_agent']['repo']
+    gpgkey node['mema_agent']['repo_key']
+    gpgcheck true
+    sensitive true
+  end
+  execute 'Update zypper cache' do
+    command "zypper refresh"
+  end
+end
