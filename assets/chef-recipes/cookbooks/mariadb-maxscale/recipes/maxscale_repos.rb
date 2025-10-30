d22f65b363eb40959c407ffbcf4ba370ea858cdc
Danil Nazarov <danil.nazarov@fruct.org>
2025-10-30 13:19:08 +0300

    Refactor recipes after apt-key removal (refs FRUC-289)

--- assets/chef-recipes/cookbooks/mariadb-maxscale/recipes/maxscale_repos.rb
+++ assets/chef-recipes/cookbooks/mariadb-maxscale/recipes/maxscale_repos.rb
@@ -1,10 +1,8 @@
 include_recipe 'clear_mariadb_repo_priorities::default'
-platform_version = node[:platform_version].to_i
-platform_family = node[:platform_family]
 #
 # MariaDB MaxScale repos
 #
-case platform_family
+case node[:platform_family]
 when "debian", "ubuntu"
   remote_file "/etc/apt/keyrings/maxscale.public" do
     source node['maxscale']['repo_key']
@@ -14,7 +12,7 @@ when "debian", "ubuntu"
   apt_repository 'maxscale' do
     uri node['maxscale']['repo']
     components node['maxscale']['components']
-    options ["signed-by=\"/etc/apt/keyrings/maxscale.public\""]
+    options ["signed-by=/etc/apt/keyrings/maxscale.public"]
     sensitive true
   end
 when "rhel", "fedora", "centos", "almalinux", "oracle"
