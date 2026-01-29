c65641e293c9ec99d4ec71e1f560f90bbd014e52
Vladislav Larionov <vladislav.larionov@fruct.org>
2026-01-29 15:10:11 +0300

    Use drone-provided repositories instead of S3 bucket (refs FRUC-331)

--- core/commands/generate_repository_partials/mdbe_ci_parser.rb
+++ core/commands/generate_repository_partials/mdbe_ci_parser.rb
@@ -6,7 +6,7 @@ require_relative 'repository_parser_core'
 module MdbeCiParser
   extend RepositoryParserCore
   DEFAULT_MDBE_VERSION = '10.5'
-  S3_VERS = ['10.6-enterprise','11.4-enterprise','11.8-enterprise']
+  S3_VERS = ['10.6-enterprise', '11.4-enterprise', '11.8-enterprise']
 
   def self.parse(config, product_version, mdbe_ci_config, log, logger)
     return [] if mdbe_ci_config.nil?
@@ -20,7 +20,7 @@ module MdbeCiParser
     )
     releases.concat(
       parse_mdbe_ci_rpm_repository_yum(config['repo']['mdbe_ci_repo'], product_version,
-                                   config['scan_mode'], auth_mdbe_ci_repo, log, logger)
+                                       config['scan_mode'], auth_mdbe_ci_repo, log, logger)
     )
     releases.concat(
       parse_mdbe_ci_deb_repository(config['repo']['mdbe_ci_repo'], product_version,
@@ -28,18 +28,20 @@ module MdbeCiParser
     )
     releases.concat(
       parse_mdbe_ci_es_repo_rpm_repository(config['repo']['es_repo'], product_version,
-                                   config['scan_mode'], auth_es_repo, log, logger)
+                                           config['scan_mode'], auth_es_repo, log, logger)
     )
     releases.concat(
       parse_mdbe_ci_es_repo_deb_repository(config['repo']['es_repo'], product_version,
-                                   config['scan_mode'], auth_es_repo, log, logger)
+                                           config['scan_mode'], auth_es_repo, log, logger)
     )
     releases.concat(parse_cs_repos(config['repo']['cs_repo']['path'],
                                    config['repo']['cs_repo']['yum_key'],
-                                   config['repo']['cs_repo']['branches'],
                                    config['repo']['cs_repo']['latest_branches'],
-                                   ))
-    releases.uniq! { |release| [release[:architecture], release[:platform], release[:platform_version], release[:product], release[:version]] }
+                                   auth_mdbe_ci_repo, logger))
+    releases.uniq! do |release|
+      [release[:architecture], release[:platform], release[:platform_version], release[:product],
+       release[:version]]
+    end
     releases
   end
 
@@ -148,83 +150,73 @@ module MdbeCiParser
     "#{add_auth_to_url(full_url, auth)}/ #{platform_and_version}/"
   end
 
-
-  def self.parse_cs_repos(url, yum_key, branches, latest_branches)
+  def self.parse_cs_repos(url, yum_key, latest_branches, auth_mdbe_ci_repo, logger)
     releases = []
-    branches.each do |branch_dir|
-      releases.concat(parse_s3_dirs(url, branch_dir, S3_VERS, yum_key))
-    end
     latest_branches.each do |branch_dir|
       S3_VERS.each do |version|
-        releases.concat(generate_s3_repositories(url, branch_dir, 'latest', version, yum_key))
+        releases.concat(generate_cspkg_latest_repositories(url, branch_dir, version,
+                                                           yum_key, auth_mdbe_ci_repo, logger))
       end
     end
     releases
   end
 
   ARCHITECTURE_DIRECTORIES = {
-    "amd64" => "amd64",
-    "aarch64" => "arm64",
-    "ppc64le" => "ppc64le"
+    'amd64' => 'amd64',
+    'aarch64' => 'arm64',
+    'ppc64le' => 'ppc64le'
   }.freeze
-  DEB_PLATFORMS = ["debian", "ubuntu"].freeze
+  DEB_PLATFORMS = %w[debian ubuntu].freeze
 
-  def self.generate_s3_repositories(repo_url, branch, version, repo_product_ver, yum_key)
+  def self.generate_cspkg_latest_repositories(repo_url, branch, s3_version, yum_key, auth, logger)
     releases = []
-    platforms = get_mdbe_platforms
-    platforms.each do |platform, platform_info|
-      ARCHITECTURE_DIRECTORIES.each do |architecture,directory|
-        base_repo_link = "#{repo_url}#{branch}/#{version}/#{repo_product_ver}/#{directory}"
-        repo = if DEB_PLATFORMS.include?(platform_info[:platform])
-          "#{base_repo_link} #{platform}/"
-        else
-          "#{base_repo_link}/#{platform}"
+    archs = retrive_archs("#{repo_url}#{branch}/latest/#{s3_version}/", auth)
+    archs.each do |arch|
+      platforms = retrive_platforms("#{repo_url}#{branch}/latest/#{s3_version}/#{arch}/", auth)
+      platforms.each do |platform|
+        platform, platform_feature = platform.split('_')
+        platform_info = get_mdbe_platforms[platform]
+        if platform_info.nil?
+          logger.write("Unknown platform #{platform}, skipped.")
+          next
         end
-
-        releases.append(
-          platform_info.merge({
-            repo: repo,
-            version: "columnstore/#{branch}/#{version}/#{repo_product_ver}",
-            product: "mdbe_ci",
-            architecture: architecture,
-            repo_key: yum_key,
-            disable_gpgcheck: true
-        }))
+        releases.append(form_repo_info(platform_info, repo_url, branch, platform, platform_feature,
+                                       arch, s3_version, yum_key))
       end
     end
     releases
   end
 
-  def self.parse_s3_dirs(repo_url, branch_dir, repo_product_vers, yum_key)
-    links = []
-    platforms = get_mdbe_platforms
-    full_url = "#{repo_url}?prefix=#{branch_dir}/&delimiter=/"
-    doc = Nokogiri.XML(URI.open(full_url))
-    doc_array = doc.at_css("ListBucketResult").children
-    full_url += "&marker="
-    while doc.at_css("IsTruncated").children.to_s == "true"
-      doc = Nokogiri.XML(URI.open(full_url + doc.at_css("NextMarker").children.to_s))
-      doc_array += doc.at_css("ListBucketResult").children
-    end
-    elements = doc_array.count
-    doc_array.each do |file|
-      file_link = file.to_s.match(/<Prefix>.*<\/Prefix>/).to_s[8..-10]
-      links.append(file_link)
-    end
+  def self.retrive_archs(link, auth)
+    perform_span_parsing(link, auth)
+  end
 
-    links.compact!
-    links.map! { |link| link.match(/(?<=(#{branch_dir})\/)[0-9]+/).to_s }
-    links = links.reject { |element| element.empty? }
+  def self.retrive_platforms(link, auth)
+    perform_span_parsing(link, auth)
+  end
 
-    releases = []
-    links.each do |version|
-      repo_product_vers.each do |repo_product_ver|
-        releases.concat(
-          generate_s3_repositories(repo_url, branch_dir, version, repo_product_ver, yum_key)
-        )
-      end
-    end
-    releases
+  def self.perform_span_parsing(link, auth)
+    doc = Nokogiri.HTML(URI.open(link,
+                                 http_basic_authentication: [auth['username'],
+                                                             auth['password']]))
+    doc.css('span.name').map { |document| document.text.sub('/', '') }
+  end
+
+  def self.form_repo_info(platform_info, repo_url, branch, platform, platform_feature, arch, s3_version, yum_key)
+    base_repo_link = "#{repo_url}#{branch}/latest/#{s3_version}/#{arch}"
+    repo = if DEB_PLATFORMS.include?(platform_info[:platform])
+             "#{base_repo_link}/ #{platform}/"
+           else
+             "#{base_repo_link}/#{platform}/"
+           end
+    platform_feature = "-#{platform_feature}" if platform_feature
+    platform_info.merge({
+                          repo: repo,
+                          version: "columnstore/#{branch}/latest#{platform_feature}/#{s3_version}",
+                          product: 'mdbe_ci',
+                          architecture: arch,
+                          repo_key: yum_key,
+                          disable_gpgcheck: true
+                        })
   end
 end
-
