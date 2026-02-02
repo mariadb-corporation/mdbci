08acb0ebee89fc2d31099d1d0c1504271139c9ba
Vladislav Larionov <vladislav.larionov@fruct.org>
2026-02-02 17:26:39 +0300

    Fix maxscale installation for debian trixie (refs FRUC-298)

--- assets/chef-recipes/cookbooks/mariadb-maxscale/recipes/maxscale_repos.rb
+++ assets/chef-recipes/cookbooks/mariadb-maxscale/recipes/maxscale_repos.rb
@@ -7,6 +7,27 @@ repo_keys = [node['maxscale']['repo_key']].flatten
 
 case node[:platform_family]
 when 'debian', 'ubuntu'
+  directory '/etc/crypto-policies/back-ends' do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    recursive true
+    action :create
+  end
+  ruby_block 'Modify the sha1 expiry politics' do
+    block do
+      config = File.read('/usr/share/apt/default-sequoia.config')
+      new_config = config.split("\n").map do |line|
+        if line.start_with?('sha1.second_preimage_resistance')
+          "sha1.second_preimage_resistance = #{Time.new.year + 1}-02-01"
+        else
+          line
+        end
+      end.join("\n")
+
+      File.write('/etc/crypto-policies/back-ends/apt-sequoia.config', new_config)
+    end
+  end
   directory '/etc/apt/keyrings' do
     owner 'root'
     group 'root'
