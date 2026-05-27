e02f4187d4cb5b0bb92016f40a37fb9953a5a654
Vladislav Larionov <vladislav.larionov@fruct.org>
2026-05-27 18:35:54 +0300

    Fix cmapi repos priority (refs FRUC-375)

--- assets/chef-recipes/cookbooks/cmapi_ci/recipes/default.rb
+++ assets/chef-recipes/cookbooks/cmapi_ci/recipes/default.rb
@@ -3,10 +3,10 @@ include_recipe 'cmapi_ci::cmapi_ci_repos'
 case node[:platform_family]
 when 'rhel', 'centos', 'almalinux', 'oracle', 'suse', 'opensuse', 'sles'
   package 'MariaDB-columnstore-cmapi' do
-    action :install
+    action :upgrade
   end
 when 'debian', 'ubuntu'
   package 'mariadb-columnstore-cmapi' do
-    action :install
+    action :upgrade
   end
 end
