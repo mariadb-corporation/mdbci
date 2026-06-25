1dc884bafc53541a39d761dcdd74d0c0521e10b0
Vladislav Larionov <vladislav.larionov@fruct.org>
2026-06-25 13:14:38 +0300

    Fix a certificate error of mariadb community product for Resolute (refs FRUC-383)

--- assets/chef-recipes/cookbooks/mariadb/recipes/mdbcrepos.rb
+++ assets/chef-recipes/cookbooks/mariadb/recipes/mdbcrepos.rb
@@ -16,18 +16,24 @@ when 'debian', 'ubuntu', 'mint'
   end
 
   repo_keys.each_with_index do |key_url, index|
-    filename = "mariadb-#{index}.public"
+    armored_filename = "mariadb-#{index}.public"
+    binary_filename = "mariadb-#{index}.gpg"
 
-    remote_file "/etc/apt/keyrings/#{filename}" do
+    remote_file "/etc/apt/keyrings/#{armored_filename}" do
       source key_url
       sensitive true
       action :create
     end
+
+    execute "convert gpg key #{index}" do
+      command "gpg --dearmor -o /etc/apt/keyrings/#{binary_filename} < /etc/apt/keyrings/#{armored_filename}"
+      creates "/etc/apt/keyrings/#{binary_filename}"
+      action :run
+    end
   end
 
   key_files = repo_keys.map.with_index do |_, index|
-    filename = "mariadb-#{index}.public"
-    "/etc/apt/keyrings/#{filename}"
+    "/etc/apt/keyrings/mariadb-#{index}.gpg"
   end
 
   apt_repository 'mariadb' do
@@ -49,6 +55,9 @@ when 'debian', 'ubuntu', 'mint'
     action :add
   end
 
+  apt_update do
+    action :update
+  end
 when 'rhel', 'fedora', 'centos', 'almalinux', 'oracle'
   if node[:platform_family] == 'rhel' && node[:platform_version].to_f >= 9.0
     execute 'Set crypto policy as LEGACY' do
